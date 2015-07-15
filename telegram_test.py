124ea4e4a3763c2bdef56832759c59d736dc2e3b
leandrotoledo <leandrotoledodesouza@gmail.com>
2015-07-15 10:18:02 -0300

    Python 2.6 unittest support

--- telegram_test.py
+++ telegram_test.py
@@ -1,5 +1,6 @@
 import unittest
+from tests.test_bot import BotTest
 
 if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover('.')
+    testsuite = unittest.TestLoader().loadTestsFromTestCase(BotTest)
     unittest.TextTestRunner(verbosity=1).run(testsuite)
