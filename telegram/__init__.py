5cd8d5b33200039eef91fbd182b2efb29b3fd155
leandrotoledo <leandrotoledodesouza@gmail.com>
2015-07-20 08:03:47 -0300

    Releasing 2.1

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -3,7 +3,7 @@
 """A library that provides a Python interface to the Telegram Bots API"""
 
 __author__ = 'leandrotoledodesouza@gmail.com'
-__version__ = '2.0'
+__version__ = '2.1'
 
 from .user import User
 from .message import Message
