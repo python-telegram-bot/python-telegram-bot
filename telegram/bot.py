3974690b907c0678326798e4472cef8420270ea9
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-11 23:43:40 -0300

    Fixes base_url when pickling object

--- telegram/bot.py
+++ telegram/bot.py
@@ -675,4 +675,5 @@ class Bot(TelegramObject):
         return data
 
     def __reduce__(self):
-        return (self.__class__, (self.token, self.base_url))
+        return (self.__class__, (self.token, 
+                                 self.base_url.replace(self.token, '')))
