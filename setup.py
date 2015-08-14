2cecca8324aa6aa995806db23c7b7ca48eec424d
Leandro Toledo <leandrotoledodesouza@gmail.com>
2015-08-14 16:36:39 -0300

    Releasing v2.6.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*paths):
 
 setup(
     name='python-telegram-bot',
-    version='2.5.3',
+    version='2.6.0',
     author='Leandro Toledo',
     author_email='leandrotoledodesouza@gmail.com',
     license='LGPLv3',
