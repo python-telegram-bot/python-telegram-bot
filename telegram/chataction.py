ccdb999e37079dd77c9720d1feaedc1704ce30eb
rahiel <rahiel@protonmail.ch>
2015-08-25 13:45:02 +0200

    add remaining chat actions

--- telegram/chataction.py
+++ telegram/chataction.py
@@ -20,7 +20,9 @@
 class ChatAction(object):
     TYPING = 'typing'
     UPLOAD_PHOTO = 'upload_photo'
-    RECORD_VIDEO = 'upload_video'
-    RECORD_AUDIO = 'upload_audio'
+    RECORD_VIDEO = 'record_video'
+    UPLOAD_VIDEO = 'upload_video'
+    RECORD_AUDIO = 'record_audio'
+    UPLOAD_AUDIO = 'upload_audio'
     UPLOAD_DOCUMENT = 'upload_document'
     FIND_LOCATION = 'find_location'
