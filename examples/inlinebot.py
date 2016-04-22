d383c10d4e3537273fe5d9f87cedfdfe6c3c3ed1
Jannes Höke <jhoeke@uni-bremen.de>
2016-04-22 15:36:46 +0200

    remove check for inline_query

--- examples/inlinebot.py
+++ examples/inlinebot.py
@@ -50,30 +50,29 @@ def escape_markdown(text):
 
 
 def inlinequery(bot, update):
-    if update.inline_query is not None and update.inline_query.query:
-        query = update.inline_query.query
-        results = list()
+    query = update.inline_query.query
+    results = list()
 
-        results.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Caps",
-                input_message_content=InputTextMessageContent(query.upper())))
+    results.append(InlineQueryResultArticle(
+            id=uuid4(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(query.upper())))
 
-        results.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Bold",
-                input_message_content=InputTextMessageContent(
-                    "*%s*" % escape_markdown(query),
-                    parse_mode=ParseMode.MARKDOWN)))
+    results.append(InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                "*%s*" % escape_markdown(query),
+                parse_mode=ParseMode.MARKDOWN)))
 
-        results.append(InlineQueryResultArticle(
-                id=uuid4(),
-                title="Italic",
-                input_message_content=InputTextMessageContent(
-                    "_%s_" % escape_markdown(query),
-                    parse_mode=ParseMode.MARKDOWN)))
+    results.append(InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                "_%s_" % escape_markdown(query),
+                parse_mode=ParseMode.MARKDOWN)))
 
-        bot.answerInlineQuery(update.inline_query.id, results=results)
+    bot.answerInlineQuery(update.inline_query.id, results=results)
 
 
 def error(bot, update, error):
