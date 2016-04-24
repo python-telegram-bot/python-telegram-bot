6cacea96f7bea1463e4b81debe76de3f27f36c9c
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-04-23 22:43:48 -0300

    No clue what's going on with travis and Venue

--- tests/test_inlinequeryresultvenue.py
+++ tests/test_inlinequeryresultvenue.py
@@ -84,10 +84,10 @@ class InlineQueryResultVenueTest(BaseTest, unittest.TestCase):
         self.assertDictEqual(venue.reply_markup.to_dict(),
                              self.reply_markup.to_dict())
 
-    # def test_venue_to_json(self):
-    #     venue = telegram.InlineQueryResultVenue.de_json(self.json_dict)
-    #
-    #     self.assertTrue(self.is_json(venue.to_json()))
+    def test_venue_to_json(self):
+        venue = telegram.InlineQueryResultVenue.de_json(self.json_dict)
+
+        self.assertTrue(self.is_json(venue.to_json()))
 
     def test_venue_to_dict(self):
         venue = telegram.InlineQueryResultVenue.de_json(
