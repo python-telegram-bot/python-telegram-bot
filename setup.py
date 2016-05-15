ac55ba007ee5ff6b3fb0331517f91cf3269a3193
leandrotoledo <leandrotoledodesouza@gmail.com>
2016-05-15 20:04:52 -0300

    Bumping version to v4.1 [ci skip]

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 '''The setup and build script for the python-telegram-bot library.'''
 
 import os
@@ -24,34 +23,32 @@ def requirements():
     return requirements_list
 
 
-setup(
-    name='python-telegram-bot',
-    version='4.0.3',
-    author='Leandro Toledo',
-    author_email='devs@python-telegram-bot.org',
-    license='LGPLv3',
-    url='https://github.com/python-telegram-bot/python-telegram-bot',
-    keywords='python telegram bot api wrapper',
-    description='A Python wrapper around the Telegram Bot API',
-    long_description=(read('README.rst')),
-    packages=find_packages(exclude=['tests*']),
-    install_requires=requirements(),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Communications :: Chat',
-        'Topic :: Internet',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-)
+setup(name='python-telegram-bot',
+      version='4.1',
+      author='Leandro Toledo',
+      author_email='devs@python-telegram-bot.org',
+      license='LGPLv3',
+      url='https://github.com/python-telegram-bot/python-telegram-bot',
+      keywords='python telegram bot api wrapper',
+      description='A Python wrapper around the Telegram Bot API',
+      long_description=(read('README.rst')),
+      packages=find_packages(exclude=['tests*']),
+      install_requires=requirements(),
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Communications :: Chat',
+          'Topic :: Internet',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],)
