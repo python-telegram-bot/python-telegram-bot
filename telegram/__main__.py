b76337de87c0242bb45022a226dd524bb83de60d
Noam Meltzer <tsnoam@gmail.com>
2016-07-09 14:40:53 +0300

    __main__.py: assist with creating issues on github
    usage:
    python -m telegram
    
    and copy/paste the output

---
+++ telegram/__main__.py
@@ -0,0 +1,18 @@
+import sys
+
+import urllib3
+import certifi
+import future
+
+from . import __version__ as telegram_ver
+
+
+def print_ver_info():
+    print('python-telegram-bot {0}'.format(telegram_ver))
+    print('urllib3 {0}'.format(urllib3.__version__))
+    print('certifi {0}'.format(certifi.__version__))
+    print('future {0}'.format(future.__version__))
+    print('Python {0}'.format(sys.version.replace('\n', ' ')))
+
+# main
+print_ver_info()
