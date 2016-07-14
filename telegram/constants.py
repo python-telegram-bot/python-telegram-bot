02b505257899d31ad84978401e881dcbffbf693e
Rahiel Kasim <rahielkasim@gmail.com>
2016-07-14 21:23:10 +0200

    document constants

--- telegram/constants.py
+++ telegram/constants.py
@@ -14,7 +14,19 @@
 #
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""Constants in the Telegram network."""
+"""Constants in the Telegram network.
+
+Attributes:
+    MAX_MESSAGE_LENGTH (int)
+    MAX_CAPTION_LENGTH (int)
+    SUPPORTED_WEBHOOK_PORTS (List[int])
+    MAX_FILESIZE_DOWNLOAD (int)
+    MAX_FILESIZE_UPLOAD (int): Official limit, the actual limit can be a bit higher.
+    MAX_MESSAGES_PER_SECOND_PER_CHAT (int): Telegram may allow short bursts that go over this
+        limit, but eventually you'll begin receiving 429 errors.
+    MAX_MESSAGES_PER_SECOND (int)
+    MAX_MESSAGES_PER_MINUTE_PER_GROUP (int)
+"""
 
 # https://core.telegram.org/method/messages.sendMessage
 MAX_MESSAGE_LENGTH = 4096
@@ -24,11 +36,10 @@ MAX_CAPTION_LENGTH = 200
 
 # constants above this line are tested
 
-MAX_MESSAGES_PER_SECOND = 30
-MAX_MESSAGES_PER_SECOND_PER_CHAT = 1
-MAX_MESSAGES_PER_MINUTE_PER_GROUP = 20
-
-MAX_FILESIZE_UPLOAD = 50E6  # (50MB)
-MAX_FILESIZE_DOWNLOAD = 20E6  # (20MB)
-
+# https://core.telegram.org/bots/faq
 SUPPORTED_WEBHOOK_PORTS = [443, 80, 88, 8443]
+MAX_FILESIZE_DOWNLOAD = int(20E6)  # (20MB)
+MAX_FILESIZE_UPLOAD = int(50E6)  # (50MB)
+MAX_MESSAGES_PER_SECOND_PER_CHAT = 1
+MAX_MESSAGES_PER_SECOND = 30
+MAX_MESSAGES_PER_MINUTE_PER_GROUP = 20
