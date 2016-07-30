02c0a75846e45b7cc87d39da324dacaef5f39ad0
Jannes Höke <jhoeke@uni-bremen.de>
2016-07-30 17:32:15 +0200

    fix create_references in webhook handler

--- telegram/utils/webhookhandler.py
+++ telegram/utils/webhookhandler.py
@@ -84,7 +84,7 @@ class WebhookHandler(BaseHTTPServer.BaseHTTPRequestHandler, object):
             self.logger.debug('Webhook received data: ' + json_string)
 
             update_dict = json.loads(json_string)
-            self.server.create_references(update_dict)
+            self.server.bot.create_references(update_dict)
             update = Update.de_json(update_dict)
 
             self.logger.debug('Received Update with ID %d on Webhook' % update.update_id)
