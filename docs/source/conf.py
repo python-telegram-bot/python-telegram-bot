1eb4b40dbf84ae89990692a5ca92ed9f5a1cf4c2
Jannes Höke <mail@jannes-hoeke.de>
2016-11-01 08:09:25 +0100

    [ci skip] docs: add logo to html and pdf builds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,7 +131,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'ptb-logo-orange.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -235,7 +235,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'ptb-logo_1024.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
