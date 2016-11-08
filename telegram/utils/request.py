1569723c2bbdf9628d209e0be32fad75d1f99afb
Eugene Lisitsky <lisitsky@gmail.com>
2016-11-09 01:00:44 +0300

    Change exception to InvalidToken in test and request util

--- telegram/utils/request.py
+++ telegram/utils/request.py
@@ -31,8 +31,8 @@ import urllib3
 from urllib3.connection import HTTPConnection
 
 from telegram import (InputFile, TelegramError)
-from telegram.error import Unauthorized, NetworkError, TimedOut, BadRequest, ChatMigrated, \
-    RetryAfter
+from telegram.error import Unauthorized, InvalidToken, NetworkError, TimedOut, BadRequest, \
+    ChatMigrated, RetryAfter
 
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 
@@ -151,7 +151,7 @@ class Request(object):
         elif resp.status == 400:
             raise BadRequest(repr(message))
         elif resp.status == 404:
-            raise TelegramError('Invalid server response')
+            raise InvalidToken()
         elif resp.status == 502:
             raise NetworkError('Bad Gateway')
         else:
