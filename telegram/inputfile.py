61c5d6ff0825ff3c4eec0814340be58c0e3ae257
Jacob Bom <bomjacob@gmail.com>
2017-06-07 13:07:45 +0200

    Remove more warnings

--- telegram/inputfile.py
+++ telegram/inputfile.py
@@ -75,7 +75,7 @@ class InputFile(object):
     def headers(self):
         """
         Returns:
-            str:
+            str
         """
         return {'User-agent': USER_AGENT, 'Content-type': self.content_type}
 
@@ -83,14 +83,14 @@ class InputFile(object):
     def content_type(self):
         """
         Returns:
-            str:
+            str
         """
         return 'multipart/form-data; boundary=%s' % self.boundary
 
     def to_form(self):
         """
         Returns:
-            str:
+            str
         """
         form = []
         form_boundary = '--' + self.boundary
@@ -118,7 +118,7 @@ class InputFile(object):
     def _parse(form):
         """
         Returns:
-            str:
+            str
         """
         if sys.version_info > (3,):
             # on Python 3 form needs to be byte encoded
