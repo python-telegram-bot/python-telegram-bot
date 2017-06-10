ab6cf0fe689bac67af86f2f0417e88e9030a48dc
Eldin <pieter.schutz@gmail.com>
2017-06-11 01:22:38 +0200

    spelling and fucking crap

--- appveyor.py
+++ appveyor.py
@@ -10,7 +10,8 @@ def main():
                }
     URL = 'https://ci.appveyor.com//api/builds'
     if pyversion=="pypy-5.3.1":
-        pr = os.environ.get('$TRAVIS_PULL_REQUEST', None)
+        pr = os.environ.get('TRAVIS_PULL_REQUEST', None)
+        print("TRAVIS_PULL_REQUEST: {}".format(pr))
         if pr:
             PAYLOAD_APPVEYOR = {
                 'accountName': 'Eldinnie',
@@ -18,16 +19,20 @@ def main():
                 'pullRequestId': pr
             }
             r = requests.post(URL, data=PAYLOAD_APPVEYOR, headers=HEADERS)
-            print(r.json)
+            print(r.json())
         else:
+            branch = os.environ.get('TRAVIS_BRANCH', None)
+            print("TRAVIS_BRANCH: {}".format(branch))
+            commit_id = os.environ.get('TRAVIS_COMMIT', None)
+            print("TRAVIS_COMMIT: {}".format(commit_id))
             PAYLOAD_APPVEYOR = {
                 'accountName': 'Eldinnie',
                 'projectSlug': 'python-telegram-bot-6akeh',
-                'branch': os.environ.get('$TRAVIS_BRANCH', None),
-                'commitId': os.environ.get('TRAVIS_COMMIT', None)
+                'branch': branch,
+                'commitId': commit_id
             }
             r = requests.post(URL, data=PAYLOAD_APPVEYOR, headers=HEADERS)
-            print(r.json)
+            print(r.json())
 
 if __name__=="__main__":
     main()
