49a8297ec2f9a9798a0d0c1c3ec1737b1d310727
Eldin <pieter.schutz@gmail.com>
2017-06-10 23:37:46 +0200

    typo

--- appveyor.py
+++ appveyor.py
@@ -2,7 +2,8 @@ import os
 
 
 def main():
-    print(os.environ.get('TRAVIS_PYTHON_VERSION', "didn't get it"))
+    pyversion = os.environ.get('TRAVIS_PYTHON_VERSION', "didn't get it")
+    print(pyversion)
 
 if __name__=="__main__":
     main()
