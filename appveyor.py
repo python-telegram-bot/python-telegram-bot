446c6eb3dc365f476e9ddf787487e7ec5bf2942f
Eldin <pieter.schutz@gmail.com>
2017-06-11 00:55:33 +0200

    python approach

--- appveyor.py
+++ appveyor.py
@@ -1,9 +1,30 @@
 import os
+import requests
 
 
 def main():
     pyversion = os.environ.get('TRAVIS_PYTHON_VERSION', "didn't get it")
-    print(pyversion)
+    HEADERS = {'Content-Type':'application/json'}
+    URL = 'https://ci.appveyor.com//api/builds'
+    if pyversion=="pypy-5.3.1":
+        pr = os.environ.get('$TRAVIS_PULL_REQUEST', None)
+        if pr:
+            PAYLOAD_APPVEYOR = {
+                'accountName': 'Eldinnie',
+                'projectSlug': 'python-telegram-bot-6akeh',
+                'pullRequestId': pr
+            }
+            r = requests.post(URL, data=PAYLOAD_APPVEYOR, headers=HEADERS)
+            print(r.json)
+        else:
+            PAYLOAD_APPVEYOR = {
+                'accountName': 'Eldinnie',
+                'projectSlug': 'python-telegram-bot-6akeh',
+                'branch': os.environ.get('$TRAVIS_BRANCH', None),
+                'commitId': os.environ.get('TRAVIS_COMMIT', None)
+            }
+            r = requests.post(URL, data=PAYLOAD_APPVEYOR, headers=HEADERS)
+            print(r.json)
 
 if __name__=="__main__":
     main()
