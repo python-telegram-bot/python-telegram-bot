9158d6cf3d865467e8ad714e6c664ab7545d07fb
Eldin <pieter.schutz@gmail.com>
2017-06-11 02:18:58 +0200

    final version>?

--- appveyor.py
+++ appveyor.py
@@ -1,8 +1,12 @@
+import json
 import os
 import requests
+import sys
+
+import time
 
 
-def main():
+def build():
     pyversion = os.environ.get('TRAVIS_PYTHON_VERSION', "didn't get it")
     HEADERS = {
         'Content-Type': 'application/json',
@@ -19,7 +23,7 @@ def main():
                 'pullRequestId': pr
             }
             r = requests.post(URL, json=PAYLOAD_APPVEYOR, headers=HEADERS)
-            print(r.json())
+            print("Started build on Appveyor")
         else:
             branch = os.environ.get('TRAVIS_BRANCH', None)
             print("TRAVIS_BRANCH: {}".format(branch))
@@ -32,7 +36,67 @@ def main():
                 'commitId': commit_id
             }
             r = requests.post(URL, json=PAYLOAD_APPVEYOR, headers=HEADERS)
-            print(r.json())
+            print("Started build on Appveyor")
+
+def wait():
+    HEADERS = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer vooe775fjw49jt0iu0d1'
+    }
+    URL = 'https://ci.appveyor.com/api/projects/Eldinnie/python-telegram-bot-6akeh'
+    print("checking if Appveyor is still running")
+    r = requests.get(url=URL, headers=HEADERS)
+    result = r.json()
+    try:
+        status = result['build']['status']
+    except KeyError:
+        status = ""
+    if status != 'running':
+        print('Appveyor is not working. Continue')
+        sys.exit()
+    else:
+        print("Appveyor is running.")
+        pr = str(os.environ.get('TRAVIS_PULL_REQUEST', None))
+        print("TRAVIS_PULL_REQUEST: {}".format(pr))
+        if pr != 'false':
+            try:
+                appveyor_pr = result['build']['pullRequestId']
+            except KeyError:
+                appveyor_pr = ''
+            if pr == appveyor_pr:
+                print("same pr in appveyor, shutting it down")
+                version = result['build']['version']
+                URL = "https://ci.appveyor.com/api/builds/Eldinnie/python-telegram-bot-6akeh/{}".format(version)
+                r = requests.delete(URL, headers=HEADERS)
+                sys.exit()
+        else:
+            branch = os.environ.get('TRAVIS_BRANCH', None)
+            appveyor_branch = result['build']['branch']
+            if branch == appveyor_branch:
+                print("New commit on the same branch, canceling build on Appveyor")
+                version = result['build']['version']
+                URL = "https://ci.appveyor.com/api/builds/Eldinnie/python-telegram-bot-6akeh/{}".format(version)
+                r = requests.delete(URL, headers=HEADERS)
+                sys.exit()
+            else:
+                print("Appveyor working, waiting for completion.")
+                while True:
+                    time.sleep(30)
+                    URL = 'https://ci.appveyor.com/api/projects/Eldinnie/python-telegram-bot-6akeh'
+                    r = requests.get(url=URL, headers=HEADERS)
+                    result = r.json()
+                    try:
+                        status = result['build']['status']
+                    except KeyError:
+                        status = ""
+                    if status != 'running':
+                        print('Appveyor is done. Continue')
+                        sys.exit()
+
 
 if __name__=="__main__":
-    main()
+    arg = sys.argv[1]
+    if arg == 'build':
+        build()
+    if arg == 'wait':
+        wait()
