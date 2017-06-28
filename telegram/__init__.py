7741638ee114a189ab607d6af591e5a2d0833d12
Rahiel Kasim <rahielkasim@gmail.com>
2017-06-28 20:38:57 +0200

    remove ReplyKeyboardHide

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -37,7 +37,7 @@ from .userprofilephotos import UserProfilePhotos
 from .keyboardbutton import KeyboardButton
 from .replymarkup import ReplyMarkup
 from .replykeyboardmarkup import ReplyKeyboardMarkup
-from .replykeyboardremove import ReplyKeyboardRemove, ReplyKeyboardHide
+from .replykeyboardremove import ReplyKeyboardRemove
 from .forcereply import ForceReply
 from .error import TelegramError
 from .files.inputfile import InputFile
