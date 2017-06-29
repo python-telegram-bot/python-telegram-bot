c75b2b88314c2d92c30075791d69729858bfbce5
Jannes Höke <jhoeke@uni-bremen.de>
2017-06-29 12:41:39 +0200

    log error trace if there is no error handler registered

--- telegram/ext/dispatcher.py
+++ telegram/ext/dispatcher.py
@@ -365,5 +365,13 @@ class Dispatcher(object):
             error (telegram.TelegramError): The Telegram error that was raised.
         """
 
-        for callback in self.error_handlers:
-            callback(self.bot, update, error)
+        if self.error_handlers:
+            for callback in self.error_handlers:
+                callback(self.bot, update, error)
+
+        else:
+            try:
+                raise error
+
+            except:
+                self.logger.exception('No error handlers are registered, logging exception...')
