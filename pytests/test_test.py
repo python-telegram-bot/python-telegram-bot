15edf67bd3a90d19a1a57f07cf9db12ef9966b51
Eldin <pieter.schutz@gmail.com>
2017-07-29 19:14:40 +0200

    need travis test

---
+++ pytests/test_test.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+
+
+@pytest.mark.timeout(2)
+def test_one():
+    time.sleep(1)
+    assert True
+
+
+@pytest.mark.timeout(2, method="thread")
+def test_two():
+    time.sleep(3)
+    assert True
+
+def test_three():
+    time.sleep(3)
+    assert True
