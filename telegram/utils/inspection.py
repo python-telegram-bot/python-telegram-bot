94031138a3986b327190e64311335318bae289d0
JosXa <joscha.goetzer@gmail.com>
2017-10-07 13:04:36 +0200

    Removed unneeded tuple return values

--- telegram/utils/inspection.py
+++ telegram/utils/inspection.py
@@ -1,10 +1,13 @@
 import inspect
 
+"""
+Reflects on a function or method to retrieve all positional and keyword arguments available.
+"""
 try:
     def inspect_arguments(func):
         args, _, _, _ = inspect.getargspec(func)
         return args
 except Warning:  # `getargspec()` is deprecated in Python3
     def inspect_arguments(func):
-        args, varargs, varkw, defaults, kwonlyargs, kwonlydefaults, annotations = inspect.getfullargspec(func)
+        args, _, _, _, _, _, _ = inspect.getfullargspec(func)
         return args
