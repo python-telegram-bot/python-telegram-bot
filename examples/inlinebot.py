9b6d857da79ce12f829888d1d357cf50b1d044c1
Simon Schürrle <sitischu@hotmail.de>
2017-10-16 08:05:00 +0200

    Used Jannes(jh0ker) suggestion for the inlinequery.py

--- examples/inlinebot.py
+++ examples/inlinebot.py
@@ -53,20 +53,23 @@ def inlinequery(bot, update):
     """Handle the inline query."""
     query = update.inline_query.query
     results = [
-        InlineQueryResultArticle(id=uuid4(),
-                                 title="Caps",
-                                 input_message_content=InputTextMessageContent(
-                                    query.upper())),
-        InlineQueryResultArticle(id=uuid4(),
-                                 title="Bold",
-                                 input_message_content=InputTextMessageContent(
-                                    "*{}*".format(escape_markdown(query)),
-                                    parse_mode=ParseMode.MARKDOWN)),
-        InlineQueryResultArticle(id=uuid4(),
-                                 title="Italic",
-                                 input_message_content=InputTextMessageContent(
-                                     "_{}_".format(escape_markdown(query)),
-                                     parse_mode=ParseMode.MARKDOWN))]
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(
+                query.upper())),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                "*{}*".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                "_{}_".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN))]
 
     update.inline_query.answer(results)
 
