7d1537f8eca57f10a05f69d2d06b78d03306616e
JosXa <joscha.goetzer@gmail.com>
2018-03-16 21:41:34 +0100

    Implemented Tools for deep linking

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,6 +16,8 @@
 #
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
+import pytest
+
 from telegram import Update
 
 from telegram import Sticker
@@ -30,6 +32,25 @@ class TestHelpers(object):
         expected_str = '\*bold\*, \_italic\_, \`code\`, \[text\_link](http://github.com/)'
 
         assert expected_str == helpers.escape_markdown(test_str)
+
+    def test_create_deep_linked_url(self):
+        username = 'JamesTheMock'
+
+        payload = "hello"
+        expected = "https://t.me/{}?start={}".format(username, payload)
+        actual = helpers.create_deep_linked_url(username, payload)
+        assert expected == actual
+
+        payload = ""
+        expected = "https://t.me/{}".format(username)
+        assert expected == helpers.create_deep_linked_url(username, payload)
+        assert expected == helpers.create_deep_linked_url(username)
+
+        with pytest.raises(ValueError):
+            helpers.create_deep_linked_url(username, 'text with spaces')
+
+        with pytest.raises(ValueError):
+            helpers.create_deep_linked_url(username, '0' * 65)
 
     def test_effective_message_type(self):
         test_message = Message(message_id=1,
