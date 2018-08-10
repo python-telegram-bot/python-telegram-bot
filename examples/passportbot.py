a637b422dcf1c6fc0a45be3dd7066209977f959c
Jasmin Bom <bomjacob@gmail.com>
2018-08-10 14:24:14 +0200

    Add better passport example

--- examples/passportbot.py
+++ examples/passportbot.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2018
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
+# Simple Bot to print/download all incoming passport data
+# This program is dedicated to the public domain under the CC0 license.
+"""
+See https://telegram.org/blog/passport for info about what telegram passport is.
+
+To try this example with simple javascript SDK do the following:
+
+1) Make sure you have openssl
+2) Generate a private key by typing this in a terminal
+> openssl genrsa 2048 > private.key
+WARNING: Keep this key private
+3) Generate a public key by typing this in a terminal
+> openssl rsa -in private.key -pubout
+4) Copy-paste the result of previous command into /setpublickey with @BotFather
+5) Add a privacy policy to your bot that details how you intend to handle this data
+6) Open examples/passportbot.html and insert your bot id and public key. Bot id is the part
+before : in your bot token
+7) Also in examples/passportbot.html decide what scopes (data) your bot needs
+8) If you upload the examples/passportbot.html file to a server, put the url in the callback
+url. For test/example purposes, example.org is fine.
+9) Make sure that the SDK file from https://github.com/TelegramMessenger/TGPassportJsSDK is
+download and in the same directory as examples/passportbot.html.
+10) Make sure the private.key from step 2 is in the same folder as this file.
+11) Start the bot by executing this file
+12) Open examples/passportbot.html in a browser and click the button
+13) This should open telegram with it's passport setup
+14) After you've authorized the request, the bot will print and download all the data and files.
+
+"""
 import logging
 
 from telegram.ext import Updater, MessageHandler, Filters
@@ -28,9 +42,13 @@ logger = logging.getLogger(__name__)
 
 
 def msg(bot, update):
+    # If we received any passport data
     passport_data = update.message.passport_data
     if passport_data:
-        print(update.message.passport_data.credentials.decrypted_data)
+        # Print the decrypted credential data
+        # For all elements
+        # Print their decrypted data
+        # Files will be downloaded to current directory
         for data in passport_data.data:
             if data.type == 'phone_number':
                 print('Phone: ', data.phone_number)
@@ -38,23 +56,31 @@ def msg(bot, update):
                 print('Email: ', data.email)
             if data.type in ('personal_details', 'passport', 'driver_license', 'identity_card',
                              'identity_passport', 'address'):
-                print(data.type, data.decrypted_data)
+                print(data.type, data.data)
             if data.type in ('utility_bill', 'bank_statement', 'rental_agreement',
                              'passport_registration', 'temporary_registration'):
                 print(data.type, len(data.files), 'files')
                 for file in data.files:
-                    print(file.get_file())
+                    actual_file = file.get_file()
+                    print(actual_file)
+                    actual_file.download()
             if data.type in ('passport', 'driver_license', 'identity_card',
                              'internal_passport'):
                 if data.front_side:
-                    print(data.type, data.front_side.get_file())
+                    file = data.front_side.get_file()
+                    print(data.type, file)
+                    file.download()
             if data.type in ('driver_license' and 'identity_card'):
                 if data.reverse_side:
-                    print(data.type, data.reverse_side.get_file())
+                    file = data.reverse_side.get_file()
+                    print(data.type, file)
+                    file.download()
             if data.type in ('passport', 'driver_license', 'identity_card',
                              'internal_passport'):
                 if data.selfie:
-                    print(data.type, data.selfie.get_file())
+                    file = data.selfie.get_file()
+                    print(data.type, file)
+                    file.download()
 
 
 def error(bot, update, error):
@@ -64,12 +90,13 @@ def error(bot, update, error):
 
 def main():
     """Start the bot."""
-    # Create the EventHandler and pass it your bot's token and private key.
-    updater = Updater("TOKEN", private_key="PRIVATE_KEY")
+    # Create the Updater and pass it your token and private key
+    updater = Updater("TOKEN", private_key=open('private.key', 'rb').read())
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    # On all messages call msg
     dp.add_handler(MessageHandler(Filters.all, msg))
 
     # log all errors
