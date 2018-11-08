c4c9e1ef483979e8fc1567bb16599ea22cacef6c
Jasmin Bom <bomjacob@gmail.com>
2018-11-08 21:51:28 +0100

    Add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -96,6 +96,7 @@ def dp(_dp):
         _dp.update_queue.get(False)
     _dp.chat_data = defaultdict(dict)
     _dp.user_data = defaultdict(dict)
+    _dp.persistence = None
     _dp.handlers = {}
     _dp.groups = []
     _dp.error_handlers = []
