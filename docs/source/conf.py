895c904829964f2857e37ecb8c320c406d14c757
Jasmin Bom <jsmnbom@gmail.com>
2019-02-17 23:57:43 +0100

    Setting needs_sphinx to 1.7.9
    so we don't have more issues with incongruent versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.5.4'  # fixes issues with autodoc-skip-member and napoleon
+needs_sphinx = '1.7.9'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
