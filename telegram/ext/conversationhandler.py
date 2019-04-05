2ed4cbd26dc97de69e040363ce7ea0794e0592a1
Loo Zheng Yuan <loozhengyuan@users.noreply.github.com>
2019-04-05 18:59:32 +0800

    Fix #1366: _trigger_timeout() missing 1 required positional argument: 'job' (#1367)
    * Fix #1366: _trigger_timeout() missing 1 required positional argument: 'job'
    
    * Add comments

--- telegram/ext/conversationhandler.py
+++ telegram/ext/conversationhandler.py
@@ -23,7 +23,7 @@ import warnings
 
 from telegram import Update
 from telegram.ext import (Handler, CallbackQueryHandler, InlineQueryHandler,
-                          ChosenInlineResultHandler)
+                          ChosenInlineResultHandler, CallbackContext)
 from telegram.utils.promise import Promise
 
 
@@ -354,12 +354,19 @@ class ConversationHandler(Handler):
             if self.persistent:
                 self.persistence.update_conversation(self.name, key, new_state)
 
-    def _trigger_timeout(self, bot, job):
+    def _trigger_timeout(self, context, job=None):
         self.logger.debug('conversation timeout was triggered!')
-        del self.timeout_jobs[job.context.conversation_key]
+        
+        # Backward compatibility with bots that do not use CallbackContext
+        if isinstance(context, CallbackContext):
+            context = context.job.context
+        else:
+            context = job.context
+        
+        del self.timeout_jobs[context.conversation_key]
         handlers = self.states.get(self.TIMEOUT, [])
         for handler in handlers:
-            check = handler.check_update(job.context.update)
+            check = handler.check_update(context.update)
             if check is not None and check is not False:
-                handler.handle_update(job.context.update, job.context.dispatcher, check)
-        self.update_state(self.END, job.context.conversation_key)
+                handler.handle_update(context.update, context.dispatcher, check)
+        self.update_state(self.END, context.conversation_key)
