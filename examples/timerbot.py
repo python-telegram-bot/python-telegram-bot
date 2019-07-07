e836ebd0084eb524f5dfe81334ff16c8750d820d
René Filip <renefilip@mail.com>
2019-07-07 12:09:14 +0200

    fix multiple timer bug

--- examples/timerbot.py
+++ examples/timerbot.py
@@ -55,9 +55,12 @@ def set_timer(update, context):
             update.message.reply_text('Sorry we can not go back to future!')
             return
 
-        # Add job to queue
-        job = context.job_queue.run_once(alarm, due, context=chat_id)
-        context.chat_data['job'] = job
+        # Add job to queue and stop current one if there is a timer already
+        if 'job' in context.chat_data:
+            old_job = context.chat_data['job']
+            old_job.schedule_removal()
+        new_job = context.job_queue.run_once(alarm, due, context=chat_id)
+        context.chat_data['job'] = new_job
 
         update.message.reply_text('Timer successfully set!')
 
