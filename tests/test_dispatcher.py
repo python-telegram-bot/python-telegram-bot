8ed64ec9eb42d14ea00caec55d1077166d14796d
poolitzer <deadpool10@web.de>
2019-08-23 16:33:25 -0700

    adding missing test

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -24,10 +24,12 @@ from time import sleep
 import pytest
 
 from telegram import TelegramError, Message, User, Chat, Update, Bot, MessageEntity
-from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext, JobQueue
+from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext, JobQueue, BasePersistence
 from telegram.ext.dispatcher import run_async, Dispatcher, DispatcherHandlerStop
 from telegram.utils.deprecate import TelegramDeprecationWarning
 from tests.conftest import create_dp
+
+from collections import defaultdict
 
 
 @pytest.fixture(scope='function')
@@ -341,6 +343,46 @@ class TestDispatcher(object):
         dp.process_update(update)
         assert passed == ['start1', 'error', err, 'start3']
         assert passed[2] is err
+
+    def test_error_while_saving_chat_data(self, dp, bot):
+        increment = []
+
+        class OwnPersistence(BasePersistence):
+            def __init__(self):
+                super().__init__()
+
+            def get_chat_data(self):
+                return defaultdict(dict)
+
+            def update_chat_data(self, chat_id, data):
+                raise Exception
+
+            def get_user_data(self):
+                return defaultdict(dict)
+
+            def update_user_data(self, user_id, data):
+                raise Exception
+
+        def start1(b, u):
+            pass
+
+        def error(b, u, e):
+            increment.append("error")
+
+        # If updating a user_data or chat_data from a persistence object throws an error,
+        # the error handler should catch it
+
+        update = Update(1, message=Message(1, User(1, "Test", False), None, Chat(1, "lala"), text='/start',
+                                           entities=[MessageEntity(type=MessageEntity.BOT_COMMAND,
+                                                                   offset=0,
+                                                                   length=len('/start'))],
+                                           bot=bot))
+        my_persistence = OwnPersistence()
+        dp = Dispatcher(bot, None, persistence=my_persistence)
+        dp.add_handler(CommandHandler('start', start1))
+        dp.add_error_handler(error)
+        dp.process_update(update)
+        assert increment == ["error", "error"]
 
     def test_flow_stop_in_error_handler(self, dp, bot):
         passed = []
