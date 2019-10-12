3318239cf649212d164d670ec3781fcf71fafb37
Paolo Lammens <lammenspaolo@gmail.com>
2019-10-12 14:11:09 +0100

    tests: refactor ``test_commandhandler.py`` (#1408)
    - Improved usage of fixtures
        - Replaced fixtures for directly callable factories where
        multiple mock objects were needed in the same test function
        - Extracted fixtures where possible (in place of literals or
        global constants)
      - Moved some fixtures to ``conftest.py`` to be used by other
      modules
      - Made a common base class for both ``TestCommandHandler`` and
      ``TestPrefixHandler``, extracting common methods, patterns and
      signatures
        - The extracted patterns in test methods have been named with
        leading ``_test``
      - Extracted other repeatedly used test utilities into functions
      (e.g. ``is_match``) and methods (e.g. ``make_default_handler``)

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,8 +16,10 @@
 #
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
+import datetime
 import os
 import sys
+import re
 from collections import defaultdict
 from queue import Queue
 from threading import Thread, Event
@@ -25,8 +27,9 @@ from time import sleep
 
 import pytest
 
-from telegram import Bot
-from telegram.ext import Dispatcher, JobQueue, Updater
+from telegram import Bot, Message, User, Chat, MessageEntity, Update, \
+    InlineQuery, CallbackQuery, ShippingQuery, PreCheckoutQuery, ChosenInlineResult
+from telegram.ext import Dispatcher, JobQueue, Updater, BaseFilter
 from tests.bots import get_bot
 
 TRAVIS = os.getenv('TRAVIS', False)
@@ -46,7 +49,7 @@ def bot_info():
 
 @pytest.fixture(scope='session')
 def bot(bot_info):
-    return Bot(bot_info['token'], private_key=PRIVATE_KEY)
+    return make_bot(bot_info)
 
 
 @pytest.fixture(scope='session')
@@ -146,3 +149,107 @@ def pytest_configure(config):
     if sys.version_info >= (3,):
         config.addinivalue_line('filterwarnings', 'ignore::ResourceWarning')
         # TODO: Write so good code that we don't need to ignore ResourceWarnings anymore
+
+
+def make_bot(bot_info):
+    return Bot(bot_info['token'], private_key=PRIVATE_KEY)
+
+
+CMD_PATTERN = re.compile(r'/[\da-z_]{1,32}(?:@\w{1,32})?')
+DATE = datetime.datetime.now()
+
+
+def make_message(text, **kwargs):
+    """
+    Testing utility factory to create a fake ``telegram.Message`` with
+    reasonable defaults for mimicking a real message.
+    :param text: (str) message text
+    :return: a (fake) ``telegram.Message``
+    """
+    return Message(message_id=1,
+                   from_user=kwargs.pop('user', User(id=1, first_name='', is_bot=False)),
+                   date=kwargs.pop('date', DATE),
+                   chat=kwargs.pop('chat', Chat(id=1, type='')),
+                   text=text,
+                   bot=kwargs.pop('bot', make_bot(get_bot())),
+                   **kwargs)
+
+
+def make_command_message(text, **kwargs):
+    """
+    Testing utility factory to create a message containing a single telegram
+    command.
+    Mimics the Telegram API in that it identifies commands within the message
+    and tags the returned ``Message`` object with the appropriate ``MessageEntity``
+    tag (but it does this only for commands).
+
+    :param text: (str) message text containing (or not) the command
+    :return: a (fake) ``telegram.Message`` containing only the command
+    """
+
+    match = re.search(CMD_PATTERN, text)
+    entities = [MessageEntity(type=MessageEntity.BOT_COMMAND,
+                              offset=match.start(0),
+                              length=len(match.group(0)))] if match else []
+
+    return make_message(text, entities=entities, **kwargs)
+
+
+def make_message_update(message, message_factory=make_message, edited=False, **kwargs):
+    """
+    Testing utility factory to create an update from a message, as either a
+    ``telegram.Message`` or a string. In the latter case ``message_factory``
+    is used to convert ``message`` to a ``telegram.Message``.
+    :param message: either a ``telegram.Message`` or a string with the message text
+    :param message_factory: function to convert the message text into a ``telegram.Message``
+    :param edited: whether the message should be stored as ``edited_message`` (vs. ``message``)
+    :return: ``telegram.Update`` with the given message
+    """
+    if not isinstance(message, Message):
+        message = message_factory(message, **kwargs)
+    update_kwargs = {'message' if not edited else 'edited_message': message}
+    return Update(0, **update_kwargs)
+
+
+def make_command_update(message, edited=False, **kwargs):
+    """
+    Testing utility factory to create an update from a message that potentially
+    contains a command. See ``make_command_message`` for more details.
+    :param message: message potentially containing a command
+    :param edited: whether the message should be stored as ``edited_message`` (vs. ``message``)
+    :return: ``telegram.Update`` with the given message
+    """
+    return make_message_update(message, make_command_message, edited, **kwargs)
+
+
+@pytest.fixture(scope='function')
+def mock_filter():
+    class MockFilter(BaseFilter):
+        def __init__(self):
+            self.tested = False
+
+        def filter(self, message):
+            self.tested = True
+
+    return MockFilter()
+
+
+def get_false_update_fixture_decorator_params():
+    message = Message(1, User(1, '', False), DATE, Chat(1, ''), text='test')
+    params = [
+        {'callback_query': CallbackQuery(1, User(1, '', False), 'chat', message=message)},
+        {'channel_post': message},
+        {'edited_channel_post': message},
+        {'inline_query': InlineQuery(1, User(1, '', False), '', '')},
+        {'chosen_inline_result': ChosenInlineResult('id', User(1, '', False), '')},
+        {'shipping_query': ShippingQuery('id', User(1, '', False), '', None)},
+        {'pre_checkout_query': PreCheckoutQuery('id', User(1, '', False), '', 0, '')},
+        {'callback_query': CallbackQuery(1, User(1, '', False), 'chat')}
+    ]
+    ids = tuple(key for kwargs in params for key in kwargs)
+    return {'params': params, 'ids': ids}
+
+
+@pytest.fixture(scope='function', **get_false_update_fixture_decorator_params())
+def false_update(request):
+    return Update(update_id=1, **request.param)
