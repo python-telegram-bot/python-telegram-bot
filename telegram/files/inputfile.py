f16d5723850665bdaee72d5cfd575c8608bb5cc1
Septatrix <24257556+Septatrix@users.noreply.github.com>
2020-01-19 13:33:23 +0100

    Remove python2 workaround in InputFile.__init__

--- telegram/files/inputfile.py
+++ telegram/files/inputfile.py
@@ -55,11 +55,7 @@ class InputFile(object):
 
         if filename:
             self.filename = filename
-        elif (hasattr(obj, 'name')
-              and not isinstance(obj.name, int)  # py3
-              and obj.name != '<fdopen>'):  # py2
-            # on py2.7, pylint fails to understand this properly
-            # pylint: disable=E1101
+        elif (hasattr(obj, 'name') and not isinstance(obj.name, int)):
             self.filename = os.path.basename(obj.name)
 
         try:
