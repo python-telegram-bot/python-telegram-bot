d96d233dc5d74a21b800ac970d6d2ef431cc58cb
Poolitzer <25934244+Poolitzer@users.noreply.github.com>
2020-01-26 13:47:52 -0800

    dropping 2.7, 3.3, 3.4 support from setup.py and README.rst (#1734)
    Fixes #1720

--- setup.py
+++ setup.py
@@ -50,10 +50,7 @@ with codecs.open('README.rst', 'r', 'utf-8') as fd:
               'Topic :: Communications :: Chat',
               'Topic :: Internet',
               'Programming Language :: Python',
-              'Programming Language :: Python :: 2',
-              'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
               'Programming Language :: Python :: 3.6',
               'Programming Language :: Python :: 3.7'
