a36ffd2c0a895a6c2a866f8a074e7549f7ebb1d4
poolitzer <25934244+poolitzer@users.noreply.github.com>
2020-02-08 19:58:13 -0800

    improving example

--- examples/pollbot.py
+++ examples/pollbot.py
@@ -3,12 +3,14 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 """
-Basic example for a bot that works with polls.
+Basic example for a bot that works with polls. Only 3 people are allowed to interact with each
+poll/quiz the bot generates.
 """
 import logging
 
-from telegram import Poll
+from telegram import Poll, ParseMode
 from telegram.ext import Updater, CommandHandler, PollAnswerHandler, PollHandler
+from telegram.utils.helpers import mention_html
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -16,57 +18,66 @@ logger = logging.getLogger(__name__)
 
 
 def start(update, context):
-    update.message.reply_text('Please select /poll to get a poll or /quiz to get a Quiz')
+    update.message.reply_text('Please select /poll to get a Poll or /quiz to get a Quiz')
 
 
 def poll(update, context):
     questions = ["Good", "Really good", "Fantastic", "Great"]
     message = context.bot.send_poll(update.effective_user.id, "how are you?", questions,
                                     is_anonymous=False, allows_multiple_answers=True)
-    context.user_data.update({"questions": questions, "message_id": message.message_id,
-                              "chat_id": update.effective_chat.id})
+    payload = {message.poll.id: {"questions": questions, "message_id": message.message_id,
+                                 "chat_id": update.effective_chat.id, "answers": 0}}
+    context.bot_data.update(payload)
 
 
 def receive_poll_answer(update, context):
+    answer = update.poll_answer
+    poll_id = answer.poll_id
     try:
-        questions = context.user_data["questions"]
-    # this means someone else answers the poll, nothing we can do with this though
+        questions = context.bot_data[poll_id]["questions"]
+    # this means this poll answer update is from an old poll, we can't do our answering then
     except KeyError:
         return
-    selected_options = update.poll_answer.option_ids
+    selected_options = answer.option_ids
     answer_string = ""
     for question_id in selected_options:
         if question_id != selected_options[-1]:
             answer_string += questions[question_id] + " and "
         else:
             answer_string += questions[question_id]
-    update.effective_user.send_message("Great, I also feel {}!".format(answer_string))
-    if "message_id" in context.user_data:
-        context.bot.stop_poll(context.user_data["chat_id"], context.user_data["message_id"])
-
-
-QUIZ = {}
+    user_mention = mention_html(update.effective_user.id, update.effective_user.full_name)
+    context.bot.send_message(context.bot_data[poll_id]["chat_id"],
+                             "{} feels {}!".format(user_mention, answer_string),
+                             parse_mode=ParseMode.HTML)
+    context.bot_data[poll_id]["answers"] += 1
+    if context.bot_data[poll_id]["answers"] == 3:
+        context.bot.stop_poll(context.bot_data[poll_id]["chat_id"],
+                              context.bot_data[poll_id]["message_id"])
 
 
 def quiz(update, context):
     questions = ["1", "2", "4", "20"]
     message = update.effective_message.reply_poll("How many eggs do you need for a cake?",
                                                   questions, type=Poll.QUIZ, correct_option_id=2)
-    QUIZ[message.poll.id] = {"chat_id": update.effective_chat.id, "message_id": message.message_id}
+    payload = {message.poll.id: {"chat_id": update.effective_chat.id,
+                                 "message_id": message.message_id}}
+    context.bot_data.update(payload)
 
 
 def receive_quiz_answer(update, context):
+    # the bot can receive closed poll updates we don't care about
     if update.poll.is_closed:
         return
     if update.poll.total_voter_count == 3:
         try:
-            quiz_data = QUIZ[update.poll.id]
-        except ValueError:
+            quiz_data = context.bot_data[update.poll.id]
+        # this means this poll answer update is from an old poll, we can't stop it then
+        except KeyError:
             return
         context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
 
 
-def help(update, context):
+def help_handler(update, context):
     update.message.reply_text("Use /quiz or /poll to test this bot.")
 
 
@@ -81,7 +92,7 @@ def main():
     updater.dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))
     updater.dispatcher.add_handler(CommandHandler('quiz', quiz))
     updater.dispatcher.add_handler(PollHandler(receive_quiz_answer))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('help', help_handler))
 
     # Start the Bot
     updater.start_polling()
