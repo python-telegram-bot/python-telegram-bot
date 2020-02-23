0800113958479aa6c4ebd6e3374a02c7a07f5929
Hinrich Mahler <hinrich.mahler@freenet.de>
2020-02-23 18:38:39 +0100

    Remove references to travis and appveyor

--- tests/test_messagequeue.py
+++ tests/test_messagequeue.py
@@ -25,7 +25,6 @@ import pytest
 import telegram.ext.messagequeue as mq
 
 
-@pytest.mark.skipif(os.getenv('APPVEYOR'), reason="On Appveyor precise timings are not accurate.")
 @pytest.mark.skipif(os.getenv('GITHUB_ACTIONS', False) and os.name == 'nt',
                     reason="On windows precise timings are not accurate.")
 class TestDelayQueue(object):
