8b4453f4387e19fab0c9da35218dcb2ab7857a58
Hinrich Mahler <hinrich.mahler@freenet.de>
2020-03-09 15:49:39 +0100

    workaround for tornado issue on win with py>=3.8

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -47,6 +47,34 @@ signalskip = pytest.mark.skipif(sys.platform == 'win32',
                                        'whole process on windows')
 
 
+if sys.platform.startswith("win") and sys.version_info >= (3, 8):
+    """set default asyncio policy to be compatible with tornado
+    Tornado 6 (at least) is not compatible with the default
+    asyncio implementation on Windows
+    Pick the older SelectorEventLoopPolicy on Windows
+    if the known-incompatible default policy is in use.
+    do this as early as possible to make it a low priority and overrideable
+    ref: https://github.com/tornadoweb/tornado/issues/2608
+    TODO: if/when tornado supports the defaults in asyncio,
+            remove and bump tornado requirement for py38
+    Copied from https://github.com/ipython/ipykernel/pull/456/
+    """
+    import asyncio
+    try:
+        from asyncio import (
+            WindowsProactorEventLoopPolicy,
+            WindowsSelectorEventLoopPolicy,
+        )
+    except ImportError:
+        pass
+        # not affected
+    else:
+        if type(asyncio.get_event_loop_policy()) is WindowsProactorEventLoopPolicy:
+            # WindowsProactorEventLoopPolicy is not compatible with tornado 6
+            # fallback to the pre-3.8 default of Selector
+            asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+
 class TestUpdater(object):
     message_count = 0
     received = None
