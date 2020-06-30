02b058ce362cb5f015597f148be8b32eb94cefb6
Bibo-Joshi <hinrich.mahler@freenet.de>
2020-06-30 22:07:38 +0200

    Refactor handling of kwargs in Bot methods (#1924)
    * Unify kwargs handling in Bot methods
    
    * Remove Request.get, make api_kwargs an explicit argument, move note to head of Bot class
    
    * Fix test_official
    
    * Update get_file methods

--- tests/test_video.py
+++ tests/test_video.py
@@ -149,10 +149,10 @@ class TestVideo:
         assert message.video == video
 
     def test_send_with_video(self, monkeypatch, bot, chat_id, video):
-        def test(_, url, data, **kwargs):
+        def test(url, data, **kwargs):
             return data['video'] == video.file_id
 
-        monkeypatch.setattr('telegram.utils.request.Request.post', test)
+        monkeypatch.setattr(bot.request, 'post', test)
         message = bot.send_video(chat_id, video=video)
         assert message
 
