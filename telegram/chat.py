165a24e13ddda1f262605ee9a9c9fd46e45a129b
NikitaPirate <59086300+NikitaPirate@users.noreply.github.com>
2020-10-18 17:15:56 +0300

    Add Convenience Properties for Service Chats and Anonymous Admins (#2147)
    Co-authored-by: Bibo-Joshi <hinrich.mahler@freenet.de>

--- telegram/chat.py
+++ telegram/chat.py
@@ -150,6 +150,21 @@ class Chat(TelegramObject):
             return "https://t.me/{}".format(self.username)
         return None
 
+    @property
+    def is_anonymous_admin(self) -> bool:
+        """:obj:`bool`: Convenience property. Returns :obj:`True`, if this chat is with is the bot
+        representing anonymous admins. This behaviour is undocumented and might be changed
+        by Telegram. """
+
+        return self.id == constants.ANONYMOUS_ADMIN_ID
+
+    @property
+    def is_service_chat(self) -> bool:
+        """:obj:`bool`: Convenience property. Returns :obj:`True`, if this chat is the Telegram
+        service chat. This behaviour is undocumented and might be changed by Telegram. """
+
+        return self.id == constants.SERVICE_CHAT_ID
+
     @classmethod
     def de_json(cls, data: JSONDict, bot: 'Bot') -> Optional['Chat']:
         data = cls.parse_data(data)
