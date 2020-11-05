7c0d7a32a12b3db05ff0858aa8c4a53629826585
Hinrich Mahler <hinrich.mahler@freenet.de>
2020-11-05 18:19:46 +0100

    Webhookinfo.ip_address

--- telegram/webhookinfo.py
+++ telegram/webhookinfo.py
@@ -30,13 +30,14 @@ class WebhookInfo(TelegramObject):
 
     Objects of this class are comparable in terms of equality. Two objects of this class are
     considered equal, if their :attr:`url`, :attr:`has_custom_certificate`,
-    :attr:`pending_update_count`, :attr:`last_error_date`, :attr:`last_error_message`,
-    :attr:`max_connections` and :attr:`allowed_updates` are equal.
+    :attr:`pending_update_count`, :attr:`ip_address`, :attr:`last_error_date`,
+    :attr:`last_error_message`, :attr:`max_connections` and :attr:`allowed_updates` are equal.
 
     Attributes:
         url (:obj:`str`): Webhook URL.
         has_custom_certificate (:obj:`bool`): If a custom certificate was provided for webhook.
         pending_update_count (:obj:`int`): Number of updates awaiting delivery.
+        ip_address (:obj:`str`): Optional. Currently used webhook IP address.
         last_error_date (:obj:`int`): Optional. Unix time for the most recent error that happened.
         last_error_message (:obj:`str`): Optional. Error message in human-readable format.
         max_connections (:obj:`int`): Optional. Maximum allowed number of simultaneous HTTPS
@@ -49,6 +50,7 @@ class WebhookInfo(TelegramObject):
         has_custom_certificate (:obj:`bool`): :obj:`True`, if a custom certificate was provided for
             webhook certificate checks.
         pending_update_count (:obj:`int`): Number of updates awaiting delivery.
+        ip_address (:obj:`str`, optional): Currently used webhook IP address.
         last_error_date (:obj:`int`, optional): Unix time for the most recent error that happened
             when trying to deliver an update via webhook.
         last_error_message (:obj:`str`, optional): Error message in human-readable format for the
@@ -69,12 +71,16 @@ class WebhookInfo(TelegramObject):
         last_error_message: str = None,
         max_connections: int = None,
         allowed_updates: List[str] = None,
+        ip_address: str = None,
         **_kwargs: Any,
     ):
         # Required
         self.url = url
         self.has_custom_certificate = has_custom_certificate
         self.pending_update_count = pending_update_count
+
+        # Optional
+        self.ip_address = ip_address
         self.last_error_date = last_error_date
         self.last_error_message = last_error_message
         self.max_connections = max_connections
@@ -84,6 +90,7 @@ class WebhookInfo(TelegramObject):
             self.url,
             self.has_custom_certificate,
             self.pending_update_count,
+            self.ip_address,
             self.last_error_date,
             self.last_error_message,
             self.max_connections,
