18ce77a90d42069f5bd047c5fe435190a7da92b2
eIGato <deathmet@list.ru>
2020-11-06 20:33:43 +0300

    Fix docs indentation

--- telegram/ext/filters.py
+++ telegram/ext/filters.py
@@ -608,25 +608,25 @@ class Filters:
 
             Note:
                 * This Filter only filters by the file ending/extension of the document,
-                    it doesn't check the validity of document.
+                  it doesn't check the validity of document.
                 * The user can manipulate the file extension of a document and
-                    send media with wrong types that don't fit to this handler.
+                  send media with wrong types that don't fit to this handler.
                 * Case insensitive by default,
-                    you may change this with the flag ``case_sensitive=True``.
+                  you may change this with the flag ``case_sensitive=True``.
                 * Extension should be passed without leading dot
-                    unless it's a part of the extension.
+                  unless it's a part of the extension.
                 * Pass :obj:`None` to filter files with no extension,
-                    i.e. without a dot in the filename.
+                  i.e. without a dot in the filename.
 
             Example:
-                ``Filters.document.file_extension("jpg")``
-                    filters files with extension ``".jpg"``.
-                ``Filters.document.file_extension(".jpg")``
-                    filters files with extension ``"..jpg"``.
-                ``Filters.document.file_extension("Dockerfile", case_sensitive=True)``
-                    filters files with extension ``".Dockerfile"`` minding the case.
-                ``Filters.document.file_extension(None)``
-                    filters files without a dot in the filename.
+                * ``Filters.document.file_extension("jpg")``
+                  filters files with extension ``".jpg"``.
+                * ``Filters.document.file_extension(".jpg")``
+                  filters files with extension ``"..jpg"``.
+                * ``Filters.document.file_extension("Dockerfile", case_sensitive=True)``
+                  filters files with extension ``".Dockerfile"`` minding the case.
+                * ``Filters.document.file_extension(None)``
+                  filters files without a dot in the filename.
             """
 
             def __init__(self, file_extension: Optional[str], case_sensitive: bool = False):
@@ -725,25 +725,25 @@ officedocument.wordprocessingml.document")``-
 
             Note:
                 * This Filter only filters by the file ending/extension of the document,
-                    it doesn't check the validity of document.
+                  it doesn't check the validity of document.
                 * The user can manipulate the file extension of a document and
-                    send media with wrong types that don't fit to this handler.
+                  send media with wrong types that don't fit to this handler.
                 * Case insensitive by default,
-                    you may change this with the flag ``case_sensitive=True``.
+                  you may change this with the flag ``case_sensitive=True``.
                 * Extension should be passed without leading dot
-                    unless it's a part of the extension.
+                  unless it's a part of the extension.
                 * Pass :obj:`None` to filter files with no extension,
-                    i.e. without a dot in the filename.
+                  i.e. without a dot in the filename.
 
             Example:
-                ``Filters.document.file_extension("jpg")``
-                    filters files with extension ``".jpg"``.
-                ``Filters.document.file_extension(".jpg")``
-                    filters files with extension ``"..jpg"``.
-                ``Filters.document.file_extension("Dockerfile", case_sensitive=True)``
-                    filters files with extension ``".Dockerfile"`` minding the case.
-                ``Filters.document.file_extension(None)``
-                    filters files without a dot in the filename.
+                * ``Filters.document.file_extension("jpg")``
+                  filters files with extension ``".jpg"``.
+                * ``Filters.document.file_extension(".jpg")``
+                  filters files with extension ``"..jpg"``.
+                * ``Filters.document.file_extension("Dockerfile", case_sensitive=True)``
+                  filters files with extension ``".Dockerfile"`` minding the case.
+                * ``Filters.document.file_extension(None)``
+                  filters files without a dot in the filename.
     """
 
     class _Animation(MessageFilter):
