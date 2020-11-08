93d9f647d013f6ad928016455b8934a5a94d7d5b
Hinrich Mahler <hinrich.mahler@freenet.de>
2020-11-08 17:06:05 +0100

    InputMedia*.caption_entities

--- tests/test_inputmedia.py
+++ tests/test_inputmedia.py
@@ -27,6 +27,7 @@ from telegram import (
     InputFile,
     InputMediaAudio,
     InputMediaDocument,
+    MessageEntity,
 )
 
 # noinspection PyUnresolvedReferences
@@ -55,6 +56,7 @@ def input_media_video(class_thumb_file):
         height=TestInputMediaVideo.height,
         duration=TestInputMediaVideo.duration,
         parse_mode=TestInputMediaVideo.parse_mode,
+        caption_entities=TestInputMediaVideo.caption_entities,
         thumb=class_thumb_file,
         supports_streaming=TestInputMediaVideo.supports_streaming,
     )
@@ -66,6 +68,7 @@ def input_media_photo(class_thumb_file):
         media=TestInputMediaPhoto.media,
         caption=TestInputMediaPhoto.caption,
         parse_mode=TestInputMediaPhoto.parse_mode,
+        caption_entities=TestInputMediaPhoto.caption_entities,
     )
 
 
@@ -75,6 +78,7 @@ def input_media_animation(class_thumb_file):
         media=TestInputMediaAnimation.media,
         caption=TestInputMediaAnimation.caption,
         parse_mode=TestInputMediaAnimation.parse_mode,
+        caption_entities=TestInputMediaAnimation.caption_entities,
         width=TestInputMediaAnimation.width,
         height=TestInputMediaAnimation.height,
         thumb=class_thumb_file,
@@ -92,6 +96,7 @@ def input_media_audio(class_thumb_file):
         title=TestInputMediaAudio.title,
         thumb=class_thumb_file,
         parse_mode=TestInputMediaAudio.parse_mode,
+        caption_entities=TestInputMediaAudio.caption_entities,
     )
 
 
@@ -102,6 +107,7 @@ def input_media_document(class_thumb_file):
         caption=TestInputMediaDocument.caption,
         thumb=class_thumb_file,
         parse_mode=TestInputMediaDocument.parse_mode,
+        caption_entities=TestInputMediaDocument.caption_entities,
         disable_content_type_detection=TestInputMediaDocument.disable_content_type_detection,
     )
 
@@ -115,6 +121,7 @@ class TestInputMediaVideo:
     duration = 5
     parse_mode = 'HTML'
     supports_streaming = True
+    caption_entities = [MessageEntity(MessageEntity.BOLD, 0, 2)]
 
     def test_expected_values(self, input_media_video):
         assert input_media_video.type == self.type_
@@ -124,6 +131,7 @@ class TestInputMediaVideo:
         assert input_media_video.height == self.height
         assert input_media_video.duration == self.duration
         assert input_media_video.parse_mode == self.parse_mode
+        assert input_media_video.caption_entities == self.caption_entities
         assert input_media_video.supports_streaming == self.supports_streaming
         assert isinstance(input_media_video.thumb, InputFile)
 
@@ -136,6 +144,9 @@ class TestInputMediaVideo:
         assert input_media_video_dict['height'] == input_media_video.height
         assert input_media_video_dict['duration'] == input_media_video.duration
         assert input_media_video_dict['parse_mode'] == input_media_video.parse_mode
+        assert input_media_video_dict['caption_entities'] == [
+            ce.to_dict() for ce in input_media_video.caption_entities
+        ]
         assert input_media_video_dict['supports_streaming'] == input_media_video.supports_streaming
 
     def test_with_video(self, video):  # noqa: F811
@@ -161,12 +172,14 @@ class TestInputMediaPhoto:
     media = "NOTAREALFILEID"
     caption = "My Caption"
     parse_mode = 'Markdown'
+    caption_entities = [MessageEntity(MessageEntity.BOLD, 0, 2)]
 
     def test_expected_values(self, input_media_photo):
         assert input_media_photo.type == self.type_
         assert input_media_photo.media == self.media
         assert input_media_photo.caption == self.caption
         assert input_media_photo.parse_mode == self.parse_mode
+        assert input_media_photo.caption_entities == self.caption_entities
 
     def test_to_dict(self, input_media_photo):
         input_media_photo_dict = input_media_photo.to_dict()
@@ -174,6 +187,9 @@ class TestInputMediaPhoto:
         assert input_media_photo_dict['media'] == input_media_photo.media
         assert input_media_photo_dict['caption'] == input_media_photo.caption
         assert input_media_photo_dict['parse_mode'] == input_media_photo.parse_mode
+        assert input_media_photo_dict['caption_entities'] == [
+            ce.to_dict() for ce in input_media_photo.caption_entities
+        ]
 
     def test_with_photo(self, photo):  # noqa: F811
         # fixture found in test_photo
@@ -195,6 +211,7 @@ class TestInputMediaAnimation:
     media = "NOTAREALFILEID"
     caption = "My Caption"
     parse_mode = 'Markdown'
+    caption_entities = [MessageEntity(MessageEntity.BOLD, 0, 2)]
     width = 30
     height = 30
     duration = 1
@@ -204,6 +221,7 @@ class TestInputMediaAnimation:
         assert input_media_animation.media == self.media
         assert input_media_animation.caption == self.caption
         assert input_media_animation.parse_mode == self.parse_mode
+        assert input_media_animation.caption_entities == self.caption_entities
         assert isinstance(input_media_animation.thumb, InputFile)
 
     def test_to_dict(self, input_media_animation):
@@ -212,6 +230,9 @@ class TestInputMediaAnimation:
         assert input_media_animation_dict['media'] == input_media_animation.media
         assert input_media_animation_dict['caption'] == input_media_animation.caption
         assert input_media_animation_dict['parse_mode'] == input_media_animation.parse_mode
+        assert input_media_animation_dict['caption_entities'] == [
+            ce.to_dict() for ce in input_media_animation.caption_entities
+        ]
         assert input_media_animation_dict['width'] == input_media_animation.width
         assert input_media_animation_dict['height'] == input_media_animation.height
         assert input_media_animation_dict['duration'] == input_media_animation.duration
@@ -239,6 +260,7 @@ class TestInputMediaAudio:
     performer = 'performer'
     title = 'title'
     parse_mode = 'HTML'
+    caption_entities = [MessageEntity(MessageEntity.BOLD, 0, 2)]
 
     def test_expected_values(self, input_media_audio):
         assert input_media_audio.type == self.type_
@@ -248,6 +270,7 @@ class TestInputMediaAudio:
         assert input_media_audio.performer == self.performer
         assert input_media_audio.title == self.title
         assert input_media_audio.parse_mode == self.parse_mode
+        assert input_media_audio.caption_entities == self.caption_entities
         assert isinstance(input_media_audio.thumb, InputFile)
 
     def test_to_dict(self, input_media_audio):
@@ -259,6 +282,9 @@ class TestInputMediaAudio:
         assert input_media_audio_dict['performer'] == input_media_audio.performer
         assert input_media_audio_dict['title'] == input_media_audio.title
         assert input_media_audio_dict['parse_mode'] == input_media_audio.parse_mode
+        assert input_media_audio_dict['caption_entities'] == [
+            ce.to_dict() for ce in input_media_audio.caption_entities
+        ]
 
     def test_with_audio(self, audio):  # noqa: F811
         # fixture found in test_audio
@@ -283,6 +309,7 @@ class TestInputMediaDocument:
     media = "NOTAREALFILEID"
     caption = "My Caption"
     parse_mode = 'HTML'
+    caption_entities = [MessageEntity(MessageEntity.BOLD, 0, 2)]
     disable_content_type_detection = True
 
     def test_expected_values(self, input_media_document):
@@ -290,6 +317,7 @@ class TestInputMediaDocument:
         assert input_media_document.media == self.media
         assert input_media_document.caption == self.caption
         assert input_media_document.parse_mode == self.parse_mode
+        assert input_media_document.caption_entities == self.caption_entities
         assert (
             input_media_document.disable_content_type_detection
             == self.disable_content_type_detection
@@ -302,6 +330,9 @@ class TestInputMediaDocument:
         assert input_media_document_dict['media'] == input_media_document.media
         assert input_media_document_dict['caption'] == input_media_document.caption
         assert input_media_document_dict['parse_mode'] == input_media_document.parse_mode
+        assert input_media_document_dict['caption_entities'] == [
+            ce.to_dict() for ce in input_media_document.caption_entities
+        ]
         assert (
             input_media_document['disable_content_type_detection']
             == input_media_document.disable_content_type_detection
@@ -325,8 +356,11 @@ class TestInputMediaDocument:
 @pytest.fixture(scope='function')  # noqa: F811
 def media_group(photo, thumb):  # noqa: F811
     return [
-        InputMediaPhoto(photo, caption='photo `1`', parse_mode='Markdown'),
+        InputMediaPhoto(photo, caption='*photo* 1', parse_mode='Markdown'),
         InputMediaPhoto(thumb, caption='<b>photo</b> 2', parse_mode='HTML'),
+        InputMediaPhoto(
+            photo, caption='photo 3', caption_entities=[MessageEntity(MessageEntity.BOLD, 0, 5)]
+        ),
     ]
 
 
@@ -336,9 +370,13 @@ class TestSendMediaGroup:
     def test_send_media_group_photo(self, bot, chat_id, media_group):
         messages = bot.send_media_group(chat_id, media_group)
         assert isinstance(messages, list)
-        assert len(messages) == 2
+        assert len(messages) == 3
         assert all([isinstance(mes, Message) for mes in messages])
         assert all([mes.media_group_id == messages[0].media_group_id for mes in messages])
+        assert all(mes.caption == f'photo {idx+1}' for idx, mes in enumerate(messages))
+        assert all(
+            mes.caption_entities == [MessageEntity(MessageEntity.BOLD, 0, 5)] for mes in messages
+        )
 
     @flaky(3, 1)
     @pytest.mark.timeout(10)
@@ -348,9 +386,13 @@ class TestSendMediaGroup:
             chat_id, media_group, disable_notification=True, reply_to_message_id=m1.message_id
         )
         assert isinstance(messages, list)
-        assert len(messages) == 2
+        assert len(messages) == 3
         assert all([isinstance(mes, Message) for mes in messages])
         assert all([mes.media_group_id == messages[0].media_group_id for mes in messages])
+        assert all(mes.caption == f'photo {idx+1}' for idx, mes in enumerate(messages))
+        assert all(
+            mes.caption_entities == [MessageEntity(MessageEntity.BOLD, 0, 5)] for mes in messages
+        )
 
     def test_send_media_group_with_thumbs(
         self, bot, chat_id, video_file, photo_file, monkeypatch  # noqa: F811
