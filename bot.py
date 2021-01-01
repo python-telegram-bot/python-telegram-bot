df2daeee3b5f1264d3596c27171316441e3fac8e
Sergey Khlebnikov <hleserg@gmail.com>
2021-01-02 00:16:33 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,62 @@
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN  = os.environ['TELEGRAM_TOKEN']
+PORT  = os.environ['PORT']
+
+# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
+#r = redis.from_url(os.environ.get("REDIS_URL"))
+
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hi!')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_webhook(listen = '127.0.0.1', port = PORT, url_path="https://hleserg-bot.herokuapp.com/")
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
