286b63f81bfebaf06987daef59f89959898c5092
Hinrich Mahler <hinrich.mahler@freenet.de>
2021-06-05 12:44:13 +0200

    Even more coverage

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -212,7 +212,7 @@ class TestUpdater:
         # that depends on this distinction works
         if ext_bot and not isinstance(updater.bot, ExtBot):
             updater.bot = ExtBot(updater.bot.token)
-        if not ext_bot and not isinstance(updater.bot, Bot):
+        if not ext_bot and not type(updater.bot) is Bot:
             updater.bot = Bot(updater.bot.token)
 
         q = Queue()
