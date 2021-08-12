be00397dd55bce4f5b14f2f64727c0e41c55706b
Stɑrry Shivɑm <starry369126@outlook.com>
2021-08-12 12:28:32 +0530

    Make BasePersistence Methods Abstract (#2624)
    * Make basepersistence methods abstractmethod
    
    Signed-off-by: starry69 <starry369126@outlook.com>
    
    Co-authored-by: Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -632,6 +632,18 @@ class TestDispatcher:
             def update_conversation(self, name, key, new_state):
                 pass
 
+            def refresh_user_data(self, user_id, user_data):
+                pass
+
+            def refresh_chat_data(self, chat_id, chat_data):
+                pass
+
+            def refresh_bot_data(self, bot_data):
+                pass
+
+            def flush(self):
+                pass
+
         def start1(b, u):
             pass
 
@@ -776,6 +788,9 @@ class TestDispatcher:
             def refresh_chat_data(self, chat_id, chat_data):
                 pass
 
+            def flush(self):
+                pass
+
         def callback(update, context):
             pass
 
@@ -843,6 +858,15 @@ class TestDispatcher:
                 pass
 
             def refresh_chat_data(self, chat_id, chat_data):
+                pass
+
+            def get_callback_data(self):
+                pass
+
+            def update_callback_data(self, data):
+                pass
+
+            def flush(self):
                 pass
 
         def callback(update, context):
