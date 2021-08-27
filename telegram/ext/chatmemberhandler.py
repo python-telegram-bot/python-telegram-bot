3d8fc0927300813538e5b7c858aff3eb9937058e
poolitzer <25934244+Poolitzer@users.noreply.github.com>
2021-08-27 15:16:18 +0200

    Fix: Adding callback signature to docstring

--- telegram/ext/chatmemberhandler.py
+++ telegram/ext/chatmemberhandler.py
@@ -39,6 +39,10 @@ class ChatMemberHandler(Handler[Update, CCT]):
     Args:
         callback (:obj:`callable`): The callback function for this handler. Will be called when
             :attr:`check_update` has determined that an update should be processed by this handler.
+            Callback signature:
+
+
+            ``def callback(update: Update, context: CallbackContext)``
 
             The return value of the callback is usually ignored except for the special case of
             :class:`telegram.ext.ConversationHandler`.
