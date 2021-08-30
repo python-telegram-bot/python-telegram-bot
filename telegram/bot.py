8f0031e9c8a733aa88cc84728e8763b5a06453f2
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2021-08-30 16:31:19 +0200

    Remove Deprecated Functionality (#2644, #2740, #2745)

--- telegram/bot.py
+++ telegram/bot.py
@@ -148,6 +148,11 @@ class Bot(TelegramObject):
         incorporated into PTB. However, this is not guaranteed to work, i.e. it will fail for
         passing files.
 
+    .. versionchanged:: 14.0
+        * Removed the deprecated methods ``kick_chat_member``, ``kickChatMember``,
+          ``get_chat_members_count`` and ``getChatMembersCount``.
+        * Removed the deprecated property ``commands``.
+
     Args:
         token (:obj:`str`): Bot's unique authentication.
         base_url (:obj:`str`, optional): Telegram Bot API service URL.
@@ -173,7 +178,6 @@ class Bot(TelegramObject):
         'private_key',
         'defaults',
         '_bot',
-        '_commands',
         '_request',
         'logger',
     )
@@ -209,7 +213,6 @@ class Bot(TelegramObject):
         self.base_url = str(base_url) + str(self.token)
         self.base_file_url = str(base_file_url) + str(self.token)
         self._bot: Optional[User] = None
-        self._commands: Optional[List[BotCommand]] = None
         self._request = request or Request()
         self.private_key = None
         self.logger = logging.getLogger(__name__)
@@ -394,26 +397,6 @@ class Bot(TelegramObject):
     def supports_inline_queries(self) -> bool:
         """:obj:`bool`: Bot's :attr:`telegram.User.supports_inline_queries` attribute."""
         return self.bot.supports_inline_queries  # type: ignore
-
-    @property
-    def commands(self) -> List[BotCommand]:
-        """
-        List[:class:`BotCommand`]: Bot's commands as available in the default scope.
-
-        .. deprecated:: 13.7
-            This property has been deprecated since there can be different commands available for
-            different scopes.
-        """
-        warnings.warn(
-            "Bot.commands has been deprecated since there can be different command "
-            "lists for different scopes.",
-            TelegramDeprecationWarning,
-            stacklevel=2,
-        )
-
-        if self._commands is None:
-            self._commands = self.get_my_commands()
-        return self._commands
 
     @property
     def name(self) -> str:
@@ -2425,36 +2408,6 @@ class Bot(TelegramObject):
         return File.de_json(result, self)  # type: ignore[return-value, arg-type]
 
     @log
-    def kick_chat_member(
-        self,
-        chat_id: Union[str, int],
-        user_id: Union[str, int],
-        timeout: ODVInput[float] = DEFAULT_NONE,
-        until_date: Union[int, datetime] = None,
-        api_kwargs: JSONDict = None,
-        revoke_messages: bool = None,
-    ) -> bool:
-        """
-        Deprecated, use :func:`~telegram.Bot.ban_chat_member` instead.
-
-        .. deprecated:: 13.7
-
-        """
-        warnings.warn(
-            '`bot.kick_chat_member` is deprecated. Use `bot.ban_chat_member` instead.',
-            TelegramDeprecationWarning,
-            stacklevel=2,
-        )
-        return self.ban_chat_member(
-            chat_id=chat_id,
-            user_id=user_id,
-            timeout=timeout,
-            until_date=until_date,
-            api_kwargs=api_kwargs,
-            revoke_messages=revoke_messages,
-        )
-
-    @log
     def ban_chat_member(
         self,
         chat_id: Union[str, int],
@@ -3283,26 +3236,6 @@ class Bot(TelegramObject):
         result = self._post('getChatAdministrators', data, timeout=timeout, api_kwargs=api_kwargs)
 
         return ChatMember.de_list(result, self)  # type: ignore
-
-    @log
-    def get_chat_members_count(
-        self,
-        chat_id: Union[str, int],
-        timeout: ODVInput[float] = DEFAULT_NONE,
-        api_kwargs: JSONDict = None,
-    ) -> int:
-        """
-        Deprecated, use :func:`~telegram.Bot.get_chat_member_count` instead.
-
-        .. deprecated:: 13.7
-        """
-        warnings.warn(
-            '`bot.get_chat_members_count` is deprecated. '
-            'Use `bot.get_chat_member_count` instead.',
-            TelegramDeprecationWarning,
-            stacklevel=2,
-        )
-        return self.get_chat_member_count(chat_id=chat_id, timeout=timeout, api_kwargs=api_kwargs)
 
     @log
     def get_chat_member_count(
@@ -5420,10 +5353,6 @@ class Bot(TelegramObject):
 
         result = self._post('getMyCommands', data, timeout=timeout, api_kwargs=api_kwargs)
 
-        if (scope is None or scope.type == scope.DEFAULT) and language_code is None:
-            self._commands = BotCommand.de_list(result, self)  # type: ignore[assignment,arg-type]
-            return self._commands  # type: ignore[return-value]
-
         return BotCommand.de_list(result, self)  # type: ignore[return-value,arg-type]
 
     @log
@@ -5480,11 +5409,6 @@ class Bot(TelegramObject):
 
         result = self._post('setMyCommands', data, timeout=timeout, api_kwargs=api_kwargs)
 
-        # Set commands only for default scope. No need to check for outcome.
-        # If request failed, we won't come this far
-        if (scope is None or scope.type == scope.DEFAULT) and language_code is None:
-            self._commands = cmds
-
         return result  # type: ignore[return-value]
 
     @log
@@ -5531,9 +5455,6 @@ class Bot(TelegramObject):
             data['language_code'] = language_code
 
         result = self._post('deleteMyCommands', data, timeout=timeout, api_kwargs=api_kwargs)
-
-        if (scope is None or scope.type == scope.DEFAULT) and language_code is None:
-            self._commands = []
 
         return result  # type: ignore[return-value]
 
@@ -5736,8 +5657,6 @@ class Bot(TelegramObject):
     """Alias for :meth:`ban_chat_member`"""
     banChatSenderChat = ban_chat_sender_chat
     """Alias for :meth:`ban_chat_sender_chat`"""
-    kickChatMember = kick_chat_member
-    """Alias for :meth:`kick_chat_member`"""
     unbanChatMember = unban_chat_member
     """Alias for :meth:`unban_chat_member`"""
     unbanChatSenderChat = unban_chat_sender_chat
@@ -5772,8 +5691,6 @@ class Bot(TelegramObject):
     """Alias for :meth:`delete_chat_sticker_set`"""
     getChatMemberCount = get_chat_member_count
     """Alias for :meth:`get_chat_member_count`"""
-    getChatMembersCount = get_chat_members_count
-    """Alias for :meth:`get_chat_members_count`"""
     getWebhookInfo = get_webhook_info
     """Alias for :meth:`get_webhook_info`"""
     setGameScore = set_game_score
