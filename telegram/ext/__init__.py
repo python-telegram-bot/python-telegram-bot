12fe0429656ba18a4d597d7b9bf217b5a0bd6739
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2021-08-30 16:31:19 +0200

    Remove Deprecated Functionality (#2644)

--- telegram/ext/__init__.py
+++ telegram/ext/__init__.py
@@ -25,7 +25,7 @@ from .dictpersistence import DictPersistence
 from .handler import Handler
 from .callbackcontext import CallbackContext
 from .contexttypes import ContextTypes
-from .dispatcher import Dispatcher, DispatcherHandlerStop, run_async
+from .dispatcher import Dispatcher, DispatcherHandlerStop
 
 from .jobqueue import JobQueue, Job
 from .updater import Updater
@@ -41,8 +41,6 @@ from .typehandler import TypeHandler
 from .conversationhandler import ConversationHandler
 from .precheckoutqueryhandler import PreCheckoutQueryHandler
 from .shippingqueryhandler import ShippingQueryHandler
-from .messagequeue import MessageQueue
-from .messagequeue import DelayQueue
 from .pollanswerhandler import PollAnswerHandler
 from .pollhandler import PollHandler
 from .chatmemberhandler import ChatMemberHandler
@@ -61,7 +59,6 @@ __all__ = (
     'ContextTypes',
     'ConversationHandler',
     'Defaults',
-    'DelayQueue',
     'DictPersistence',
     'Dispatcher',
     'DispatcherHandlerStop',
@@ -74,7 +71,6 @@ __all__ = (
     'JobQueue',
     'MessageFilter',
     'MessageHandler',
-    'MessageQueue',
     'PersistenceInput',
     'PicklePersistence',
     'PollAnswerHandler',
@@ -87,5 +83,4 @@ __all__ = (
     'TypeHandler',
     'UpdateFilter',
     'Updater',
-    'run_async',
 )
