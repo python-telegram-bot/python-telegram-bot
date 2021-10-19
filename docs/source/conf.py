41d4ad379c68904ed79ff8b10ee821e6c13f0278
eldad_c <eldbud@gmail.com>
2021-10-19 19:54:02 +0300

    added APScheduler to intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,10 @@ extensions = [
 ]
 
 # Use intersphinx to reference the python builtin library docs
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'APScheduler': ('https://apscheduler.readthedocs.io/en/3.x/', None)
+}
 
 # Don't show type hints in the signature - that just makes it hardly readable
 # and we document the types anyway
