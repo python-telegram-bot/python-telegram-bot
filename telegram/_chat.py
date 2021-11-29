47060fa8ddfd0833bb5d744f9f9cfdbeb27b015f
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2021-11-29 20:32:26 +0100

    Convenience Functionality for ChatInviteLinks (#2782)

--- telegram/_chat.py
+++ telegram/_chat.py
@@ -1625,7 +1625,7 @@ class Chat(TelegramObject):
 
     def edit_invite_link(
         self,
-        invite_link: str,
+        invite_link: Union[str, 'ChatInviteLink'],
         expire_date: Union[int, datetime] = None,
         member_limit: int = None,
         timeout: ODVInput[float] = DEFAULT_NONE,
@@ -1662,7 +1662,7 @@ class Chat(TelegramObject):
 
     def revoke_invite_link(
         self,
-        invite_link: str,
+        invite_link: Union[str, 'ChatInviteLink'],
         timeout: ODVInput[float] = DEFAULT_NONE,
         api_kwargs: JSONDict = None,
     ) -> 'ChatInviteLink':
