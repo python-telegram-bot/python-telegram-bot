89d53105041203b6971764d57ae3ac7c25e3e9f9
Harshil <37377066+harshil21@users.noreply.github.com>
2021-12-05 12:42:14 +0400

    Use __all__ Consistently (#2805)

--- telegram/_version.py
+++ telegram/_version.py
@@ -18,7 +18,8 @@
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 # pylint: disable=missing-module-docstring
 
+__version__ = '13.11'
+
 from telegram import constants
 
-__version__ = '13.11'
 bot_api_version = constants.BOT_API_VERSION  # pylint: disable=invalid-name
