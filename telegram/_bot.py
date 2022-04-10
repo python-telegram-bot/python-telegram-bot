8b5900bd036493d5c5a129060c2208fec4fd80f6
Harshil <37377066+harshil21@users.noreply.github.com>
2022-04-10 19:13:23 +0530

    removed old note in restrict_chat_member

--- telegram/_bot.py
+++ telegram/_bot.py
@@ -4792,12 +4792,7 @@ class Bot(TelegramObject, AbstractAsyncContextManager):
         """
         Use this method to restrict a user in a supergroup. The bot must be an administrator in
         the supergroup for this to work and must have the appropriate admin rights. Pass
-        :obj:`True` for all boolean parameters to lift restrictions from a user.
-
-        Note:
-            Since Bot API 4.4, :meth:`restrict_chat_member` takes the new user permissions in a
-            single argument of type :class:`telegram.ChatPermissions`. The old way of passing
-            parameters will not keep working forever.
+        :obj:`True` for all boolean parameters in :class:`telegram.ChatPermissions` to lift restrictions from a user.
 
         Args:
             chat_id (:obj:`int` | :obj:`str`): Unique identifier for the target chat or username
