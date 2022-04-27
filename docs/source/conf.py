303502611510f344aea36e3e8c02f8b82f367be2
Harshil <37377066+harshil21@users.noreply.github.com>
2022-04-27 18:57:11 +0530

    add icon to ptb website and change tg link to channel

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -160,9 +160,9 @@ html_theme_options = {
                     'Transition-guide-to-Version-20.0">wiki</a>.',
 
     "footer_icons":[{
-        # Telegram group logo
-        "name": "Telegram",
-        "url": "https://t.me/pythontelegrambotgroup/",
+        # Telegram channel logo
+        "name": "Telegram Channel",
+        "url": "https://t.me/pythontelegrambotchannel/",
         # Following svg is from https://react-icons.github.io/react-icons/search?q=telegram
         "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" ' \
                 'viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">'\
@@ -180,7 +180,7 @@ html_theme_options = {
     },
     {   # Github logo
         "name": "GitHub",
-        "url": "https://github.com/python-telegram-bot/python-telegram-bot",
+        "url": "https://github.com/python-telegram-bot/python-telegram-bot/",
         "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" ' \
                 'viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 '\
                 '2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.4' \
@@ -194,6 +194,35 @@ html_theme_options = {
         ,
         "class": "",
     },
+    {   # PTB website logo - globe
+        "name": "python-telegram-bot website",
+        "url": "https://python-telegram-bot.org/",
+        "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" ' \
+                'viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">'\
+                '<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 ' \
+                '1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.53' \
+                '9c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 ' \
+                '3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 ' \
+                '9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 ' \
+                '12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h' \
+                '2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.03' \
+                '5.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409' \
+                'c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.31' \
+                '2.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.28' \
+                '2.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 ' \
+                '8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1' \
+                ' 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm' \
+                '6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 ' \
+                '8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 ' \
+                '1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm' \
+                '3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a1' \
+                '3.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.25' \
+                '8-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.6' \
+                '94.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.4' \
+                '18.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 ' \
+                '8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z"></path></svg>',
+        "class": "",
+    }
     ]
 }
 
