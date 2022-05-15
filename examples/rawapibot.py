3013870c1f873f9ff49692eca035b122a19d817f
Harshil <37377066+harshil21@users.noreply.github.com>
2022-05-15 17:38:40 +0530

    Add Version Check to Examples (#3036)

--- examples/rawapibot.py
+++ examples/rawapibot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# pylint: disable=wrong-import-position
 """Simple Bot to reply to Telegram messages.
 
 This is built on the API wrapper, see echobot.py to see the same example built
@@ -9,6 +10,19 @@ import asyncio
 import logging
 from typing import NoReturn
 
+from telegram import __version__ as TG_VER
+
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://github.com/python-telegram-bot/python-telegram-bot/tree/v{TG_VER}/examples"
+    )
 from telegram import Bot
 from telegram.error import Forbidden, NetworkError
 
