604836654328479e7fce033d88c781a8d905171c
Yagel Ashkenazi <123yagel@gmail.com>
2022-05-17 15:56:40 +0300

    EasyConversationHandler added

---
+++ examples/easyconversationbot.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument, wrong-import-position
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple example of EasyConversationBot.
+The bot let user add family members and print them, in simple, functional way - without state machines.
+"""
+
+import logging
+
+from telegram.ext._easyconversationhandler import EasyConversationHandler
+
+from telegram.ext import Application, CommandHandler
+
+# Enable logging
+from telegram.ext.filters import Regex
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+SIBLINGS_KEY, CHILDREN_KEY = 1, 2
+
+
+async def collect_user_inputs(context, num_items: int):
+    """
+    Collects a list of strings from the user in maximum length of `num_items`
+    """
+    inputs = []
+    for _ in range(num_items):
+        # await as in regular reply_text for network request
+        await context.last_update.message.reply_text('Enter input, or /done')
+
+        # Here is the magic!
+        # We can get new updates on the go
+        # Just remember to await it. This waits for the user's input, and can be for long time.
+        #  - But your functional logic stays simple and saves state!
+        update = await context.get_update()
+
+        if update.message.text == '/done':
+            break
+        inputs.append(update.message.text)
+    return inputs
+
+
+async def collect_number(context):
+    """
+    Collects an int from the user
+    """
+    while True:
+        update = await context.get_update()
+        if update.message.text.isdigit() or True:
+            return int(update.message.text)
+        await update.message.reply_text('This is not a number. Enter a number:')
+        continue
+
+
+async def collect_list(context, list_name):
+    """
+    Collects list from user. First prompts with `list_name`
+    """
+    await context.last_update.message.reply_text(f'How much {list_name}?')
+    how_much = await collect_number(context)
+    ret = await collect_user_inputs(context, how_much)
+    await context.last_update.message.reply_text(f'{list_name}: {ret}')
+    return ret
+
+
+async def siblings_callback(context):
+    await context.get_update()  # Ignore first message (/siblings)
+
+    # We can easily call this function multiple times in multiple logic paths
+    # We need to await our functions too
+    context.user_data[SIBLINGS_KEY] = await collect_list(context, 'siblings')
+
+    await print_help(context.last_update, context)
+
+
+async def children_callback(context):
+    await context.get_update()  # Ignore first message
+    context.user_data[CHILDREN_KEY] = await collect_list(context, 'children')
+    await print_help(context.last_update, context)
+
+
+# Simple command handlers callbacks
+
+async def print_data(update, context):
+    await update.message.reply_text(f'Your siblings: {context.user_data.get(SIBLINGS_KEY, "Not set yet")}')
+    await update.message.reply_text(f'Your children: {context.user_data.get(CHILDREN_KEY, "Not set yet")}')
+
+
+async def print_help(update, context):
+    await update.message.reply_text(f'Type /siblings, /children or /print')
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("5395331154:AAEEX6bcmL7Jm7pFYxxy3NL7QKlmDGpfXkk").build()
+
+    application.add_handler(CommandHandler('print', print_data))
+    application.add_handler(CommandHandler('help', print_help))
+    application.add_handler(CommandHandler('start', print_help))
+
+    application.add_handler(EasyConversationHandler(siblings_callback, first_message_filter=Regex('/siblings')))
+    application.add_handler(EasyConversationHandler(children_callback, first_message_filter=Regex('/children')))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
---
+++ telegram/ext/_easyconversationhandler.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+#
+# A library that provides a Python interface to the Telegram Bot API
+# Copyright (C) 2015-2022
+# Leandro Toledo de Souza <devs@python-telegram-bot.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+# This file is written by Yagel Ashkenazi <123yagel@gmail.com>
+
+import asyncio
+from ctypes import Union
+from sys import stderr
+from types import GeneratorType
+from typing import Callable, Any
+
+from telegram import Update
+from telegram.ext import BaseHandler
+from telegram.ext._utils.types import CCT, UT
+from telegram.ext.filters import BaseFilter
+
+
+class _EasyBotConversation:
+    def __init__(self, main_callback, error_callback):
+        self.main_callback = main_callback
+        self.error_callback = error_callback
+        self._asyncio_task: Union[GeneratorType, None] = None
+        self._semaphore = asyncio.Semaphore(0)
+        self._updates = []
+
+    async def _callback_wrapper(self, callback, context):
+        try:
+            await callback(context)
+        except Exception as e:
+            await self.error_callback(e, context)
+
+    def start_bot(self, context):
+        async def get_update():
+            await self._semaphore.acquire()
+            context.last_update = self._updates.pop()  # Eww
+            return context.last_update
+
+        context.get_update = get_update  # context new function TODO: in new Context class or so
+
+        self._asyncio_task = asyncio.create_task(self._callback_wrapper(self.main_callback, context))
+
+    def running(self):
+        return self._asyncio_task is not None and not self._asyncio_task.done()
+
+    def got_update(self, update, context):
+        self._updates.insert(0, update)
+        self._semaphore.release()
+        if not self.running():
+            self.start_bot(context)
+
+
+class EasyConversationHandler(BaseHandler[Update, CCT]):
+    def __init__(
+            self,
+            main_callback: Callable[[CCT], Any],
+            first_message_filter: BaseFilter = None,
+            error_callback: Callable[[CCT, Exception], Any] = None
+    ):
+        super(EasyConversationHandler, self).__init__(callback=lambda u, c: '')
+        self.main_callback = main_callback
+
+        if first_message_filter is None:
+            first_message_filter = BaseFilter()
+        if error_callback is None:
+            error_callback = self.default_error_callback
+
+        self.error_callback = error_callback
+        self.first_message_filter = first_message_filter
+
+        self._conversations = {}
+
+    def check_update(self, update: Update) -> bool:
+        if update.effective_chat.id in self._conversations and self._conversations[update.effective_chat.id].running():
+            return True
+        if self.first_message_filter.check_update(update):
+            return True
+        return False
+
+    @staticmethod
+    async def default_error_callback(e: Exception, context):
+        import traceback
+        stderr.write('No error handler set for EasyConversationHandler, so printing error')
+        stderr.write(''.join(traceback.format_exception(None, e, e.__traceback__)))
+
+    async def handle_update(
+            self,
+            update: UT,
+            application: "Application",
+            check_result: object,
+            context: CCT,
+    ):
+        """
+        This method is called if it was determined that an update should indeed
+        be handled by this instance. Calls :attr:`callback` along with its respectful
+        arguments. To work with the :class:`telegram.ext.ConversationHandler`, this method
+        returns the value returned from :attr:`callback`.
+        Note that it can be overridden if needed by the subclassing handler.
+
+        Args:
+            update (:obj:`str` | :class:`telegram.Update`): The update to be handled.
+            application (:class:`telegram.ext.Application`): The calling application.
+            check_result (:class:`object`): The result from :meth:`check_update`.
+            context (:class:`telegram.ext.CallbackContext`): The context as provided by
+                the application.
+
+        """
+        chat_id = update.effective_chat.id
+        if chat_id not in self._conversations:
+            # First time here?
+            self._conversations[chat_id] = _EasyBotConversation(self.main_callback, self.error_callback)
+
+        self._conversations[chat_id].got_update(update, context)
