a17a4c6c8f82f24eac00e167aa259e210c9ac4a9
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2022-05-26 19:15:54 +0200

    Handle List-Valued Attributes More Consistently (#3057)

--- telegram/_games/game.py
+++ telegram/_games/game.py
@@ -63,8 +63,9 @@ class Game(TelegramObject):
             game message. Can be automatically edited to include current high scores for the game
             when the bot calls :meth:`telegram.Bot.set_game_score`, or manually edited
             using :meth:`telegram.Bot.edit_message_text`.
-        text_entities (List[:class:`telegram.MessageEntity`]): Optional. Special entities that
+        text_entities (List[:class:`telegram.MessageEntity`]): Special entities that
             appear in text, such as usernames, URLs, bot commands, etc.
+            This list is empty if the message does not contain text entities.
         animation (:class:`telegram.Animation`): Optional. Animation that will be displayed in the
             game message in chats. Upload via `BotFather <https://t.me/BotFather>`_.
 
@@ -182,7 +183,7 @@ class Game(TelegramObject):
 
         return {
             entity: self.parse_text_entity(entity)
-            for entity in (self.text_entities or [])
+            for entity in self.text_entities
             if entity.type in types
         }
 
