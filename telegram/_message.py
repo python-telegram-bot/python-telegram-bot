37dd56d517852e1823f9d038dc723a17869a58f3
YD <ydglitch@gmail.com>
2022-06-01 13:39:06 +0000

    docstring fix

--- telegram/_message.py
+++ telegram/_message.py
@@ -575,11 +575,10 @@ class Message(TelegramObject):
     @property
     def id(self) -> int:  # pylint: disable=invalid-name
         """
-
         .. versionchanged:: 20.0
-            :obj: `int` : Convenience property. This is to reduce errors when users
-            use Message.id instead of Message.message_id which is inconsistent with most
-            of the other properties which follow a similar pattern.
+          :obj: `int` : Convenience property. This is to reduce errors when users
+          use Message.id instead of Message.message_id which is inconsistent with most
+          of the other properties which follow a similar pattern.
 
         """
         return self.message_id
