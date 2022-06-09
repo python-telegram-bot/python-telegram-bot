d6e95f1974ae2c3f9c1810307b892cc78463d235
Poolitzer <github@poolitzer.eu>
2022-06-09 17:07:28 +0200

    Improve Some Unit Tests (#3026)
    Co-authored-by: Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>

--- tests/test_chatphoto.py
+++ tests/test_chatphoto.py
@@ -83,7 +83,7 @@ class TestChatPhoto:
 
         new_file = await bot.get_file(chat_photo.small_file_id)
 
-        assert isinstance(new_file.file_id, str) and new_file.file_id != ""
+        assert new_file.file_unique_id == chat_photo.small_file_unique_id
         assert new_file.file_path.startswith("https://")
 
         await new_file.download(jpg_file)
@@ -92,7 +92,7 @@ class TestChatPhoto:
 
         new_file = await bot.get_file(chat_photo.big_file_id)
 
-        assert isinstance(new_file.file_id, str) and new_file.file_id != ""
+        assert new_file.file_unique_id == chat_photo.big_file_unique_id
         assert new_file.file_path.startswith("https://")
 
         await new_file.download(jpg_file)
