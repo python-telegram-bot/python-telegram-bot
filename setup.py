7a7465e8e213ee6b3b6e44987f4b68990b5e0174
Poolitzer <github@poolitzer.eu>
2022-06-12 15:35:58 +0200

    Fix: python_requires ammending

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def get_setup_kwargs(raw=False):
                 'Programming Language :: Python :: 3.8',
                 'Programming Language :: Python :: 3.9',
             ],
-            python_requires='>=3.6'
+            python_requires='>=3.7'
         )
 
     return kwargs
