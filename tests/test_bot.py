4611defd4727f040e91226e31942e7725a08570d
Harshil <37377066+harshil21@users.noreply.github.com>
2022-07-10 03:01:16 +0400

    iterate through enum members for testing

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -958,24 +958,9 @@ class TestBot:
         assert not unprotected_dice.has_protected_content
 
     @flaky(3, 1)
-    @pytest.mark.parametrize(
-        "chat_action",
-        [
-            ChatAction.FIND_LOCATION,
-            ChatAction.RECORD_VIDEO,
-            ChatAction.RECORD_VIDEO_NOTE,
-            ChatAction.RECORD_VOICE,
-            ChatAction.TYPING,
-            ChatAction.UPLOAD_DOCUMENT,
-            ChatAction.UPLOAD_PHOTO,
-            ChatAction.UPLOAD_VIDEO,
-            ChatAction.UPLOAD_VIDEO_NOTE,
-            ChatAction.UPLOAD_VOICE,
-            ChatAction.CHOOSE_STICKER,
-        ],
-    )
-    async def test_send_chat_action(self, bot, chat_id, chat_action):
-        assert await bot.send_chat_action(chat_id, chat_action)
+    async def test_send_chat_action(self, bot, chat_id):
+        for action in ChatAction:
+            assert await bot.send_chat_action(chat_id, action)
         with pytest.raises(BadRequest, match="Wrong parameter action"):
             await bot.send_chat_action(chat_id, "unknown action")
 
