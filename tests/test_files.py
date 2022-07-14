d7e00daaff9304349834484731752e8e4eb8fb78
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2022-07-14 15:50:29 +0200

    more test updating

--- tests/test_files.py
+++ tests/test_files.py
@@ -43,23 +43,43 @@ class TestFiles:
         assert telegram._utils.files.is_local_file(string) == expected
 
     @pytest.mark.parametrize(
-        "string,expected",
+        "string,expected_local,expected_non_local",
         [
-            (data_file("game.gif"), data_file("game.gif").as_uri()),
-            (TEST_DATA_PATH, TEST_DATA_PATH),
-            ("file://foobar", "file://foobar"),
-            (str(data_file("game.gif")), data_file("game.gif").as_uri()),
-            (str(TEST_DATA_PATH), str(TEST_DATA_PATH)),
-            (data_file("game.gif"), data_file("game.gif").as_uri()),
-            (TEST_DATA_PATH, TEST_DATA_PATH),
+            (data_file("game.gif"), data_file("game.gif").as_uri(), InputFile),
+            (TEST_DATA_PATH, TEST_DATA_PATH, TEST_DATA_PATH),
+            ("file://foobar", "file://foobar", ValueError),
+            (str(data_file("game.gif")), data_file("game.gif").as_uri(), InputFile),
+            (str(TEST_DATA_PATH), str(TEST_DATA_PATH), str(TEST_DATA_PATH)),
             (
+                "https:/api.org/file/botTOKEN/document/file_3",
                 "https:/api.org/file/botTOKEN/document/file_3",
                 "https:/api.org/file/botTOKEN/document/file_3",
             ),
         ],
+        ids=[
+            "Path(local_file)",
+            "Path(directory)",
+            "file_uri",
+            "str-path local file",
+            "str-path directory",
+            "URL",
+        ],
     )
-    def test_parse_file_input_string(self, string, expected):
-        assert telegram._utils.files.parse_file_input(string) == expected
+    def test_parse_file_input_string(self, string, expected_local, expected_non_local):
+        assert telegram._utils.files.parse_file_input(string, local_mode=True) == expected_local
+
+        if expected_non_local is InputFile:
+            assert isinstance(
+                telegram._utils.files.parse_file_input(string, local_mode=False), InputFile
+            )
+        elif expected_non_local is ValueError:
+            with pytest.raises(ValueError):
+                telegram._utils.files.parse_file_input(string, local_mode=False)
+        else:
+            assert (
+                telegram._utils.files.parse_file_input(string, local_mode=False)
+                == expected_non_local
+            )
 
     def test_parse_file_input_file_like(self):
         source_file = data_file("game.gif")
