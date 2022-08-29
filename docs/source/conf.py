7c8142628ced7e86222fd3aedeb3c2f56e246dfd
Harshil <37377066+harshil21@users.noreply.github.com>
2022-08-30 03:48:24 +0530

    get started on improving results

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -218,6 +218,8 @@ html_css_files = [
     "style_mermaid_diagrams.css",
     "style_search_box.css",
 ]
+html_js_files = ["sort_search_results.js"]
+
 html_permalinks_icon = "¶"  # Furo's default permalink icon is `#`` which doesn't look great imo.
 
 # Output file base name for HTML help builder.
