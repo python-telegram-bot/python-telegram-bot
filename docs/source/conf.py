29bb0a85539eb98b2f67a69073920bda0e5fdcb8
Harshil <37377066+harshil21@users.noreply.github.com>
2022-09-04 22:43:12 +0530

    trigger re-run 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -213,10 +213,7 @@ html_favicon = "ptb-logo_1024.ico"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = [
-    "style_external_link.css",
-    "style_mermaid_diagrams.css",
-]
+html_css_files = ["style_external_link.css", "style_mermaid_diagrams.css"]
 
 html_permalinks_icon = "¶"  # Furo's default permalink icon is `#`` which doesn't look great imo.
 
