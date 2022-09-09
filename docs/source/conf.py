17064037a485f6d01f389ee2f2af855cdb5b6919
Harshil <37377066+harshil21@users.noreply.github.com>
2022-09-10 02:24:12 +0530

    trigger rebuild

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -256,7 +256,7 @@ latex_logo = "ptb-logo_1024.png"
 # (source start file, name, description, authors, manual section).
 man_pages = [(master_doc, "python-telegram-bot", "python-telegram-bot Documentation", [author], 1)]
 
-# rtd_sphinx_search_file_type = "un-minified"  # Configuration for rtd-sphinx-search
+# rtd_sphinx_search_file_type = "un-minified"  # Configuration for furo-sphinx-search
 
 # -- Options for Texinfo output -------------------------------------------
 
