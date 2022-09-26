3fe67f5942bc47c009ccf129d696b87c53501121
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2022-09-26 20:30:07 +0200

    pytz -> datetime.tzinfz

--- telegram/_utils/datetime.py
+++ telegram/_utils/datetime.py
@@ -83,9 +83,9 @@ def to_float_timestamp(
             a :obj:`datetime.datetime` object), :paramref:`reference_timestamp` is not relevant
             and so its value should be :obj:`None`. If this is not the case, a :exc:`ValueError`
             will be raised.
-        tzinfo (:obj:`pytz.BaseTzInfo`, optional): If :paramref:`time_object` is a naive object
+        tzinfo (:class:`datetime.tzinfo`, optional): If :paramref:`time_object` is a naive object
             from the :mod:`datetime` module, it will be interpreted as this timezone. Defaults to
-            ``pytz.utc``.
+            ``pytz.utc``, if available, and :attr:`datetime.timezone.utc` otherwise.
 
             Note:
                 Only to be used by ``telegram.ext``.
