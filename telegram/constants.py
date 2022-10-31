98d81a6322d874ef0e2e87021d6ea0e4cd46a4d0
Biruk <biruk.alamirew77@gmail.com>
2022-10-31 10:27:49 +0300

    update: moved the change to untested constants

--- telegram/constants.py
+++ telegram/constants.py
@@ -654,9 +654,9 @@ class MessageLimit(IntEnum):
     """:obj:`int`: Maximum number of characters for a text message."""
     CAPTION_LENGTH = 1024
     """:obj:`int`: Maximum number of characters for a message caption."""
+    # constants above this line are tested
     DEEP_LINK_LENGTH = 64
     """:obj:`int`: Maximum number of characters for a deep link."""
-    # constants above this line are tested
     MESSAGE_ENTITIES = 100
     """:obj:`int`: Maximum number of entities that can be displayed in a message. Further entities
     will simply be ignored by Telegram.
