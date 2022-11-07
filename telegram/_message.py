9fab68e49510ed8e623897941121f8742c80863e
Robi <53259730+RobiMez@users.noreply.github.com>
2022-11-07 17:22:01 +0300

    dupe

--- telegram/_message.py
+++ telegram/_message.py
@@ -269,8 +269,6 @@ class Message(TelegramObject):
             or from anonymous administrators, information about the original sender chat.
         forward_from_message_id (:obj:`int`): Optional. For forwarded channel posts, identifier of
             the original message in the channel.
-        forward_sender_name (:obj:`str` :noindex: ): Optional. Sender's name for messages
-            forwarded from users who disallow adding a link to their account in forwarded messages.
         forward_date (:class:`datetime.datetime`): Optional. For forwarded messages, date the
             original message was sent in Unix time. Converted to :class:`datetime.datetime`.
         is_automatic_forward (:obj:`bool`): Optional. :obj:`True`, if the message is a channel
