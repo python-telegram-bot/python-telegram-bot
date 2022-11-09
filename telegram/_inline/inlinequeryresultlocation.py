f1e0e4acbe75b627138ad99fc6b7c0e51d6c7e9b
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-11-09 19:57:59 +0300

    fix(`Location` etc) create constants in classes, link to `constants.py`
    note that this approach leads to similar constants
    in multiple classes

--- telegram/_inline/inlinequeryresultlocation.py
+++ telegram/_inline/inlinequeryresultlocation.py
@@ -18,12 +18,12 @@
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 """This module contains the classes that represent Telegram InlineQueryResultLocation."""
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar
 
+from telegram import constants
 from telegram._inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 from telegram._inline.inlinequeryresult import InlineQueryResult
 from telegram._utils.types import JSONDict
-from telegram.constants import InlineQueryResultType
 
 if TYPE_CHECKING:
     from telegram import InputMessageContent
@@ -41,19 +41,20 @@ class InlineQueryResultLocation(InlineQueryResult):
         longitude (:obj:`float`): Location longitude in degrees.
         title (:obj:`str`): Location title.
         horizontal_accuracy (:obj:`float`, optional): The radius of uncertainty for the location,
-            measured in meters; 0-:tg-const:`telegram.constants.LocationLimit.HORIZONTAL_ACCURACY`.
+            measured in meters; 0-
+            :tg-const:`telegram.InlineQueryResultLocation.HORIZONTAL_ACCURACY`.
         live_period (:obj:`int`, optional): Period in seconds for which the location will be
             updated, should be between
-            :tg-const:`telegram.constants.LocationLimit.MIN_LIVE_PERIOD` and
-            :tg-const:`telegram.constants.LocationLimit.MAX_LIVE_PERIOD`.
+            :tg-const:`telegram.InlineQueryResultLocation.MIN_LIVE_PERIOD` and
+            :tg-const:`telegram.InlineQueryResultLocation.MAX_LIVE_PERIOD`.
         heading (:obj:`int`, optional): For live locations, a direction in which the user is
             moving, in degrees. Must be between
-            :tg-const:`telegram.constants.LocationLimit.MIN_HEADING` and
-            :tg-const:`telegram.constants.LocationLimit.MAX_HEADING` if specified.
+            :tg-const:`telegram.InlineQueryResultLocation.MIN_HEADING` and
+            :tg-const:`telegram.InlineQueryResultLocation.MAX_HEADING` if specified.
         proximity_alert_radius (:obj:`int`, optional): For live locations, a maximum distance
             for proximity alerts about approaching another chat member, in meters. Must be
-            between :tg-const:`telegram.constants.LocationLimit.MIN_PROXIMITY_ALERT_RADIUS`
-            and :tg-const:`telegram.constants.LocationLimit.MAX_PROXIMITY_ALERT_RADIUS`
+            between :tg-const:`telegram.InlineQueryResultLocation.MIN_PROXIMITY_ALERT_RADIUS`
+            and :tg-const:`telegram.InlineQueryResultLocation.MAX_PROXIMITY_ALERT_RADIUS`
             if specified.
         reply_markup (:class:`telegram.InlineKeyboardMarkup`, optional): Inline keyboard attached
             to the message.
@@ -121,7 +122,7 @@ class InlineQueryResultLocation(InlineQueryResult):
         api_kwargs: JSONDict = None,
     ):
         # Required
-        super().__init__(InlineQueryResultType.LOCATION, id, api_kwargs=api_kwargs)
+        super().__init__(constants.InlineQueryResultType.LOCATION, id, api_kwargs=api_kwargs)
         self.latitude = latitude
         self.longitude = longitude
         self.title = title
@@ -138,3 +139,18 @@ class InlineQueryResultLocation(InlineQueryResult):
         self.proximity_alert_radius = (
             int(proximity_alert_radius) if proximity_alert_radius else None
         )
+
+    HORIZONTAL_ACCURACY: ClassVar[int] = constants.LocationLimit.HORIZONTAL_ACCURACY
+    """:const:`telegram.constants.LocationLimit.HORIZONTAL_ACCURACY`"""
+    MIN_HEADING: ClassVar[int] = constants.LocationLimit.MIN_HEADING
+    """:const:`telegram.constants.LocationLimit.MIN_HEADING`"""
+    MAX_HEADING: ClassVar[int] = constants.LocationLimit.MAX_HEADING
+    """:const:`telegram.constants.LocationLimit.MAX_HEADING`"""
+    MIN_LIVE_PERIOD: ClassVar[int] = constants.LocationLimit.MIN_LIVE_PERIOD
+    """:const:`telegram.constants.LocationLimit.MIN_LIVE_PERIOD`"""
+    MAX_LIVE_PERIOD: ClassVar[int] = constants.LocationLimit.MAX_LIVE_PERIOD
+    """:const:`telegram.constants.LocationLimit.MAX_LIVE_PERIOD`"""
+    MIN_PROXIMITY_ALERT_RADIUS: ClassVar[int] = constants.LocationLimit.MIN_PROXIMITY_ALERT_RADIUS
+    """:const:`telegram.constants.LocationLimit.MIN_PROXIMITY_ALERT_RADIUS`"""
+    MAX_PROXIMITY_ALERT_RADIUS: ClassVar[int] = constants.LocationLimit.MAX_PROXIMITY_ALERT_RADIUS
+    """:const:`telegram.constants.LocationLimit.MAX_PROXIMITY_ALERT_RADIUS`"""
