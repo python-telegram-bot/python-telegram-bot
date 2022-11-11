1ebb561f2e2fba6f282da576124fbfa65038a48d
poolitzer <25934244+Poolitzer@users.noreply.github.com>
2022-11-11 13:39:32 +0100

    Fix: Pycharm messing with me

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -2494,6 +2494,32 @@ class TestBot:
         assert message.entities == entities
 
     @pytest.mark.flaky(3, 1)
+    async def test_send_message_to_topic(self, bot, forum_group_id):
+        # TODO rework when new methods and attributes become available
+        test_string = "Topics are forever"
+
+        result = await bot._post(
+            "createForumTopic",
+            {"chat_id": forum_group_id, "name": "Is just a yellow lemon tree"},
+        )
+
+        message_thread_id = result["message_thread_id"]
+
+        message = await bot.send_message(
+            chat_id=forum_group_id, text=test_string, message_thread_id=message_thread_id
+        )
+
+        assert message.text == test_string
+        assert message.is_topic_message is True
+        assert message.message_thread_id == message_thread_id
+
+        result = await bot._post(
+            "deleteForumTopic",
+            {"chat_id": forum_group_id, "message_thread_id": message_thread_id},
+        )
+        assert result is True
+
+    @pytest.mark.flaky(3, 1)
     @pytest.mark.parametrize("default_bot", [{"parse_mode": "Markdown"}], indirect=True)
     async def test_send_message_default_parse_mode(self, default_bot, chat_id):
         test_string = "Italic Bold Code"
