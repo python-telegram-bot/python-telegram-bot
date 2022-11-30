42bd6e4c9e357c5107e03c691ace138042d654f1
Aditya Yadav <adityayadav11082@gmail.com>
2022-11-30 09:43:37 +0530

    fix another test

--- tests/test_applicationbuilder.py
+++ tests/test_applicationbuilder.py
@@ -51,7 +51,7 @@ def builder():
 @pytest.mark.skipif(TEST_WITH_OPT_DEPS, reason="Optional dependencies are installed")
 class TestApplicationBuilderNoOptDeps:
     def test_init(self, builder):
-        builder.token("token")
+        builder.token("token").job_queue(None)
         app = builder.build()
         assert app.job_queue is None
 
