cce57ae81d4519981fe2b5fb48ec5ba48d173037
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-12-05 19:49:35 +0300

    unify args and attrs in `MaskPosition`

--- telegram/_files/sticker.py
+++ telegram/_files/sticker.py
@@ -307,9 +307,11 @@ class MaskPosition(TelegramObject):
         point (:obj:`str`): The part of the face relative to which the mask should be placed.
             One of :attr:`FOREHEAD`, :attr:`EYES`, :attr:`MOUTH`, or :attr:`CHIN`.
         x_shift (:obj:`float`): Shift by X-axis measured in widths of the mask scaled to the face
-            size, from left to right.
+            size, from left to right. For example, choosing ``-1.0`` will place mask just to the
+            left of the default mask position.
         y_shift (:obj:`float`): Shift by Y-axis measured in heights of the mask scaled to the face
-            size, from top to bottom.
+            size, from top to bottom. For example, ``1.0`` will place the mask just below the
+            default mask position.
         scale (:obj:`float`): Mask scaling coefficient. For example, ``2.0`` means double size.
 
     """
