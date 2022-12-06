8b81e1f4b908ced6dd1ee42032c9d3ade3b5a109
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-12-06 20:14:37 +0300

    `Animation`: unify args and attrs

--- telegram/_files/animation.py
+++ telegram/_files/animation.py
@@ -44,7 +44,8 @@ class Animation(_BaseThumbedMedium):
         file_size (:obj:`int`, optional): File size in bytes.
 
     Attributes:
-        file_id (:obj:`str`): File identifier.
+        file_id (:obj:`str`): Identifier for this file, which can be used to download
+            or reuse the file.
         file_unique_id (:obj:`str`): Unique identifier for this file, which
             is supposed to be the same over time and for different bots.
             Can't be used to download or reuse the file.
