d08fb9ae8b6de63d698c743c9ed28f1c516e630b
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-12-09 19:13:29 +0300

    `Invoice`: unify args and attrs
    I think the 2nd part of docstring for `start_parameter`
     can be useful to understand this attr, despite
     the phrase addressing the generation of the Invoice

--- telegram/_payment/invoice.py
+++ telegram/_payment/invoice.py
@@ -48,9 +48,15 @@ class Invoice(TelegramObject):
     Attributes:
         title (:obj:`str`): Product name.
         description (:obj:`str`): Product description.
-        start_parameter (:obj:`str`): Unique bot deep-linking parameter.
+        start_parameter (:obj:`str`): Unique bot deep-linking parameter that can be used to
+            generate this invoice.
         currency (:obj:`str`): Three-letter ISO 4217 currency code.
-        total_amount (:obj:`int`): Total price in the smallest units of the currency.
+        total_amount (:obj:`int`): Total price in the smallest units of the currency (integer, not
+            float/double). For example, for a price of US$ 1.45 ``amount`` is ``145``. See the
+            ``exp`` parameter in
+            `currencies.json <https://core.telegram.org/bots/payments/currencies.json>`_,
+            it shows the number of digits past the decimal point for each currency
+            (2 for the majority of currencies).
 
     """
 
