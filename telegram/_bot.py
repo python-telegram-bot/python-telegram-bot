fdc69542d0283517e955a9175d736a89ef5ac32c
Aditya Yadav <adityayadav11082@gmail.com>
2023-01-13 23:01:31 +0530

    sync with tg docs

--- telegram/_bot.py
+++ telegram/_bot.py
@@ -4679,9 +4679,9 @@ class Bot(TelegramObject, AbstractAsyncContextManager):
             can_pin_messages (:obj:`bool`, optional): Pass :obj:`True`, if the administrator can
                 pin messages, supergroups only.
             can_promote_members (:obj:`bool`, optional): Pass :obj:`True`, if the administrator can
-                add new administrators with a subset of his own privileges or demote administrators
-                that he has promoted, directly or indirectly (promoted by administrators that were
-                appointed by him).
+                add new administrators with a subset of their own privileges or demote administrators
+                that they have promoted, directly or indirectly (promoted by administrators that
+                were appointed by the user).
             can_manage_topics (:obj:`bool`, optional): Pass :obj:`True`, if the user is
                 allowed to create, rename, close, and reopen forum topics; supergroups only.
 
