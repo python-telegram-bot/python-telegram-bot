4cfc7d467baa47726d5699ca52a529c081cab6b9
hyfc <myloxyloto2012@gmail.com>
2023-02-07 21:32:56 +0000

    Reformat `test_called_by_file_in_symlink`

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -31,31 +31,31 @@ class TestStack:
         frame = inspect.currentframe()
         file = Path(__file__)
         assert was_called_by(frame, file)
-    
+
     def test_called_by_file_in_symlink(self):
         tests_folder = Path(__file__).parent
         project_root = tests_folder.parent
-        temp_symlink = project_root/"temp_tests"
+        temp_symlink = project_root / "temp_tests"
         dummy_file_content = "import inspect\ndef dummy_func():\
             \n    return inspect.currentframe() "
         try:
             # Set up a call from a different file in a symlink directory,
             # then test it with its resolved path.
-            # Here we expect `was_called_by` to recognize 
+            # Here we expect `was_called_by` to recognize
             # "../temp_tests/dummy.py" as same as "../tests/dummy".
             os.symlink(tests_folder, temp_symlink)
-            with open(temp_symlink/"dummy.py", 'w') as f:
+            with open(temp_symlink / "dummy.py", 'w') as f:
                 f.write(dummy_file_content)
             from temp_tests.dummy import dummy_func
 
             frame = dummy_func()
-            file = tests_folder/"dummy.py"
+            file = tests_folder / "dummy.py"
             assert was_called_by(frame, file)
         finally:
             if os.path.exists(temp_symlink):
                 os.unlink(temp_symlink)
-            if os.path.exists(tests_folder/"dummy.py"):
-                os.remove(tests_folder/"dummy.py")
+            if os.path.exists(tests_folder / "dummy.py"):
+                os.remove(tests_folder / "dummy.py")
 
     # Testing a call by a different file is somewhat hard but it's covered in
     # TestUpdater/Application.test_manual_init_warning
