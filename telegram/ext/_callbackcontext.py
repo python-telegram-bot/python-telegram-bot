fd4445de36bc71eb5798b549da42a8a3c2975479
poolitzer <github@poolitzer.eu>
2023-03-11 12:17:50 +0100

    Fix: NEXT.VERSION

--- telegram/ext/_callbackcontext.py
+++ telegram/ext/_callbackcontext.py
@@ -306,7 +306,7 @@ class CallbackContext(Generic[BT, UD, CD, BD]):
 
                 .. versionadded:: 20.0
 
-                .. versionchanged:: 20.1.1
+                .. versionchanged:: NEXT.VERSION
                     Accepts :class:`asyncio.Future` and generator-based coroutine functions.
         Returns:
             :class:`telegram.ext.CallbackContext`
