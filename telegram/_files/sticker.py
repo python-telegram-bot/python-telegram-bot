a6d47fb4b0e1ba615297fee60a041a9555423850
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-13 21:31:55 +0300

    fix `Sticker`, `StickerSet`: process & test "thumb" in `.de_json()`
    all tests are now passing

--- telegram/_files/sticker.py
+++ telegram/_files/sticker.py
@@ -193,6 +193,7 @@ class Sticker(_BaseThumbedMedium):
         if not data:
             return None
 
+        data["thumb"] = PhotoSize.de_json(data.get("thumb"), bot)
         data["thumbnail"] = PhotoSize.de_json(data.get("thumbnail"), bot)
         data["mask_position"] = MaskPosition.de_json(data.get("mask_position"), bot)
         data["premium_animation"] = File.de_json(data.get("premium_animation"), bot)
