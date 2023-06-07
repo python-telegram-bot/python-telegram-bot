63fd846233b97199bb0e32f398159b403d32434b
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-06-07 23:32:04 +0300

    Set `httpx` Logging Level to Warning in Examples (#3746)

--- examples/nestedconversationbot.py
+++ examples/nestedconversationbot.py
@@ -45,6 +45,9 @@ from telegram.ext import (
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
 # State definitions for top level conversation
--- examples/rawapibot.py
+++ examples/rawapibot.py
@@ -30,6 +30,9 @@ from telegram.error import Forbidden, NetworkError
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
 
