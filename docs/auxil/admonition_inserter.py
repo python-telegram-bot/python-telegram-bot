b8da91e1919dd09fe104c22d882d20620481e4c4
Seonzzang2 <136895049+Seonzzang2@users.noreply.github.com>
2023-06-20 17:43:36 +0900

    Revert "fix error with `Literal`, causing tests/docs to fail (#3764)"
    This reverts commit 27a8683e7089d4350660333a6fd1cabf48caf63a.

--- docs/auxil/admonition_inserter.py
+++ docs/auxil/admonition_inserter.py
@@ -528,11 +528,7 @@ class AdmonitionInserter:
         # For custom generics like telegram.ext._application.Application[~BT, ~CCT, ~UD...].
         # This must come before the check for isinstance(type) because GenericAlias can also be
         # recognized as type if it belongs to <class 'types.GenericAlias'>.
-        elif str(type(arg)) in (
-            "<class 'typing._GenericAlias'>",
-            "<class 'types.GenericAlias'>",
-            "<class 'typing._LiteralGenericAlias'>",
-        ):
+        elif str(type(arg)) in ("<class 'typing._GenericAlias'>", "<class 'types.GenericAlias'>"):
             if "telegram" in str(arg):
                 # get_origin() of telegram.ext._application.Application[~BT, ~CCT, ~UD...]
                 # will produce <class 'telegram.ext._application.Application'>
