892b61fa153af7a21a41102e0351389273d4a9d3
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-11-01 23:33:17 +0100

    more doc adaptions

--- telegram/_bot.py
+++ telegram/_bot.py
@@ -4148,7 +4148,8 @@ class Bot(TelegramObject, contextlib.AbstractAsyncContextManager["Bot"]):
         api_kwargs: Optional[JSONDict] = None,
     ) -> Union[Message, bool]:
         """
-        Use this method to edit animation, audio, document, photo, or video messages. If a message
+        Use this method to edit animation, audio, document, photo, or video messages, or to add
+        media to text messages. If a message
         is part of a message album, then it can be edited only to an audio for audio albums, only
         to a document for document albums and to a photo or a video otherwise. When an inline
         message is edited, a new file can't be uploaded; use a previously uploaded file via its
