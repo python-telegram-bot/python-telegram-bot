3f70a4312d8a5f8c7912095fa35e111534cb774c
Snehashish Biswas <coderrx06@gmail.com>
2024-11-19 00:24:08 +0530

    Update paymentbot.py

--- examples/paymentbot.py
+++ examples/paymentbot.py
@@ -21,7 +21,7 @@ from telegram.ext import (
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# Set a higher logging level for httpx to avoid logging every GET and POST request
+# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
