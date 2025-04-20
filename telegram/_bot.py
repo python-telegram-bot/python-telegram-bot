ea609c956e4d112a9b55a3e1315c4389d738171e
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2025-04-20 10:59:26 +0200

    can_delete_outgoing_messages → can_delete_sent_messages https://t.me/bot_api_changes/241

--- telegram/_bot.py
+++ telegram/_bot.py
@@ -9462,7 +9462,7 @@ READ_BUSINESS_MESSAGE_ACTIVITY_TIMEOUT` seconds.
     ) -> bool:
         """
         Delete messages on behalf of a business account. Requires the
-        :attr:`~telegram.BusinessBotRights.can_delete_outgoing_messages` business bot right to
+        :attr:`~telegram.BusinessBotRights.can_delete_sent_messages` business bot right to
         delete messages sent by the bot itself, or the
         :attr:`~telegram.BusinessBotRights.can_delete_all_messages` business bot right to delete
         any message.
