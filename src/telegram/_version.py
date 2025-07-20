05816fe85179c73fc4e88bdfd7ad879c382b05b7
Bibo-Joshi <22366557+Bibo-Joshi@users.noreply.github.com>
2025-07-20 21:43:17 +0200

    Bump Version to v22.3

--- src/telegram/_version.py
+++ src/telegram/_version.py
@@ -51,6 +51,6 @@ class Version(NamedTuple):
 
 
 __version_info__: Final[Version] = Version(
-    major=22, minor=2, micro=0, releaselevel="final", serial=0
+    major=22, minor=3, micro=0, releaselevel="final", serial=0
 )
 __version__: Final[str] = str(__version_info__)
